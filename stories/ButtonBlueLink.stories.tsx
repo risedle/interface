c4c1576dbf1e9b3a1155a63a6c0f910d5354d6fc
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 23:10:19 +0700

    ❇️ Storybook: Add ButtonBlueLink story

---
+++ stories/ButtonBlueLink.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, Story } from "@storybook/react";
+
+import ButtonBlueLink from "../components/ButtonBlueLink";
+
+export default {
+    component: ButtonBlueLink,
+    title: "Risedle/Buttons/ButtonBlueLink",
+} as Meta;
+
+const Template: Story<{
+    title: string;
+    url: string;
+}> = (args) => <ButtonBlueLink title={args.title} url={args.url} />;
+
+//Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = { title: "Deposit", url: "/lend/deposit" };
