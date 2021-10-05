daf0b06518d430fb167b8193d58d3efed2abdd1c
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 23:34:53 +0700

    ❇️ Storybook: Add DetailCard story

---
+++ stories/DetailCard.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, Story } from "@storybook/react";
+
+import DetailCard, { DetailCardListItem } from "../components/DetailCard";
+
+export default {
+    component: DetailCard,
+    title: "Risedle/Details/DetailCard",
+} as Meta;
+
+const Template: Story<{
+    items: Array<DetailCardListItem>;
+}> = (args) => <DetailCard items={args.items} />;
+
+//Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {
+    items: [
+        {
+            title: "Your balance",
+            value: "0.00 rvUSDC",
+        },
+        {
+            title: "Available to withdraw",
+            value: "0.00 rvUSDC",
+        },
+        {
+            title: "Total value",
+            value: "0.00 USDC",
+        },
+    ],
+};
