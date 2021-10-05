cf01475c5b567a573c7bdc804840cf5b1884a4d1
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 18:38:16 +0700

    ❇️ Storybook: Add ButtonBlueSecondary story

---
+++ stories/ButtonBlueSecondary.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, Story } from "@storybook/react";
+
+import ButtonBlueSecondary from "../components/ButtonBlueSecondary";
+
+export default {
+    component: ButtonBlueSecondary,
+    title: "Risedle/Buttons/ButtonBlueSecondary",
+} as Meta;
+
+const Template: Story<{
+    label: string;
+}> = (args) => <ButtonBlueSecondary>{args.label}</ButtonBlueSecondary>;
+
+//Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = { label: "Invest" };
