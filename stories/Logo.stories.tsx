810d009e7602f363cf3bf417caffd50cc84bb711
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 15:07:47 +0700

    ❇️ Storybook: Add Logo story

---
+++ stories/Logo.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, Story } from "@storybook/react";
+
+import Logo from "../components/Logo";
+
+export default {
+    component: Logo,
+    title: "Risedle/Logo/Logo",
+} as Meta;
+
+const Template: Story<{}> = () => <Logo />;
+
+//Each story then reuses that template
+export const Default = Template.bind({});
