cd94df79a4b4a2d73f4efe36778f85ffb1e20f4f
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 13:14:19 +0700

    ❇️ Storybook: Add ButtonGrey storybook

---
+++ stories/ButtonGrey.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, Story } from "@storybook/react";
+
+import ButtonGrey from "../components/ButtonGrey";
+
+export default {
+    component: ButtonGrey,
+    title: "Risedle/Buttons/ButtonGrey",
+} as Meta;
+
+const Template: Story<{
+    label: string;
+}> = (args) => <ButtonGrey>{args.label}</ButtonGrey>;
+
+//Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = { label: "Rinkeby" };
