78fce1a12093112a4100c0a91b2958b27255299b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 13:43:06 +0700

    ❇️ Components: Add ButtonOutlineCircle

---
+++ components/ButtonOutlineCircle.tsx
@@ -0,0 +1,45 @@
+import type { FunctionComponent } from "react";
+import Image from "next/image";
+
+/**
+ * ButtonOutlineCircleProps is a React Component properties that passed to React
+ * Component Button
+ */
+type ButtonOutlineCircleProps = {
+    icon: StaticImageData;
+    onClick?: () => void;
+};
+
+/**
+ * ButtonOutlineCircle is just yet another react component
+ *
+ * @link https://fettblog.eu/typescript-react/components/#functional-components
+ */
+const ButtonOutlineCircle: FunctionComponent<ButtonOutlineCircleProps> = ({
+    icon,
+    onClick,
+}) => {
+    const tailwind = [
+        "text-white",
+        "font-extrabold",
+        "text-sm",
+        "rounded-full",
+        "flex items-center justify-center",
+        "hover:opacity-95",
+        "transform active:scale-95",
+        "transition duration-300 ease-in-out",
+        "border border-opacity-20 border-white",
+        "hover:border-opacity-30",
+    ];
+    return (
+        <button
+            className={tailwind.join(" ")}
+            onClick={onClick}
+            style={{ height: "40px", width: "40px" }}
+        >
+            <Image src={icon} />
+        </button>
+    );
+};
+
+export default ButtonOutlineCircle;
