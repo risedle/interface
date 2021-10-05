9ff1e37e03fb11a4825b3e3bff6b4006ebdadbd1
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 11:16:49 +0700

    ❇️ Setup Tailwind and Google Font for Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+// Import tailwind in storybook preview
+import "tailwindcss/tailwind.css";
+
+// Setup Next.js Image for storybook
+// https://xenox.dev/next-image-with-storybookjs/
+import * as NextImage from "next/image";
+
+Object.defineProperty(NextImage, "default", {
+    configurable: true,
+    value: (props) => <img {...props} />,
+});
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+    backgrounds: {
+        default: "black",
+        values: [{ name: "black", value: "#000000" }],
+    },
+};
