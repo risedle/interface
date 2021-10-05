5d3f056a10855b53d76583bebc2330e85fdf2eb9
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 23:34:29 +0700

    ❇️ Components: Add DetailCard component

---
+++ components/DetailCard.tsx
@@ -0,0 +1,47 @@
+import type { FunctionComponent } from "react";
+import Image from "next/image";
+
+// Represents pair of title and value to displayed
+export type DetailCardListItem = {
+    title: string;
+    value: string;
+};
+
+/**
+ * DetailCardProps is a React Component properties that passed to React
+ * Component Button
+ */
+type DetailCardProps = {
+    items: Array<DetailCardListItem>;
+};
+
+/**
+ * DetailCard is just yet another react component
+ *
+ * @link https://fettblog.eu/typescript-react/components/#functional-components
+ */
+const DetailCard: FunctionComponent<DetailCardProps> = ({ items }) => {
+    const list = items.map((item) => {
+        return (
+            <div>
+                <p className="text-sm font-semibold text-grey m-0 leading-normal">
+                    {item.title}
+                </p>
+                <h3 className="text-white text-2xl font-extrabold m-0 leading-normal">
+                    {item.value}
+                </h3>
+            </div>
+        );
+    });
+
+    return (
+        <div
+            className="flex flex-col bg-gradient-to-t from-grey-100 rounded-2xl p-6 gap gap-y-4"
+            style={{ maxWidth: "480px" }}
+        >
+            {list}
+        </div>
+    );
+};
+
+export default DetailCard;
