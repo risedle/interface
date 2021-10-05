e981f4dd107a2414ec0de27b9184fed4872f879c
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-05 15:30:47 +0700

    ❇️ Component: Add menu component

---
+++ components/Menu.tsx
@@ -0,0 +1,45 @@
+import type { FunctionComponent } from "react";
+import { Fragment } from "react";
+import Link from "next/link";
+
+/**
+ * MenuProps is a React Component properties that passed to React
+ * Component Button
+ */
+type MenuProps = {};
+
+/**
+ * Menu is just yet another react component
+ *
+ * @link https://fettblog.eu/typescript-react/components/#functional-components
+ */
+const Menu: FunctionComponent<MenuProps> = ({}) => {
+    const text = "font-inter font-extrabold text-white text-opacity-50";
+    const hover = "hover:text-opacity-100";
+    const transition = "transition duration-300 ease-in-out";
+    const classNames = [text, hover, transition].join(" ");
+    return (
+        <Fragment>
+            <div className="flex flex-row gap gap-x-6">
+                <Link href="/">
+                    <a
+                        className={classNames}
+                        style={{ fontSize: "15px", lineHeight: "21px" }}
+                    >
+                        Invest
+                    </a>
+                </Link>
+                <Link href="/earn">
+                    <a
+                        className={classNames}
+                        style={{ fontSize: "15px", lineHeight: "21px" }}
+                    >
+                        Earn
+                    </a>
+                </Link>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Menu;
