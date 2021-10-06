3fb20481b93c155b07ac247ff5ee82e9757a08a4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-06 14:57:30 +0700

    ❇️ Page: Lend - Get the data from chain

--- pages/lend/index.tsx
+++ pages/lend/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+// Import the useDapp
+import { useContractCalls, useTokenBalance, useEthers } from "@usedapp/core";
+import { utils } from "ethers";
+import { Contract } from "@ethersproject/contracts";
+
 // Import components
 import Favicon from "../../components/Favicon";
 import Navigation from "../../components/Navigation";
@@ -10,32 +15,124 @@ import DetailCard from "../../components/DetailCard";
 // PNG files
 import USDC_ICON from "../../public/USDC_ICON.png";
 
+// Import ABIs
+import RisedleVaultABI from "../../abis/RisedleVaultABI.json";
+const vaultInterface = new utils.Interface(RisedleVaultABI);
+const vaultContractAddress = "0x84917B264a8b50B0dA6A6e6a2bC422F02001Bf98";
+
 const Lend: NextPage = () => {
-    // TODO (bayu):
-    // 1. Get supply APY data
-    // 2. Get TVL data
-    // 3. Get user balance on the vault token
-    // 4. Get available to withdraw
-    // 5. Get the exchange rate of vault token
+    // Setup hooks
+    const { account } = useEthers();
+
+    // Read data from chain
+    const results = useContractCalls([
+        {
+            abi: vaultInterface,
+            address: vaultContractAddress,
+            method: "getSupplyRatePerSecondInEther",
+            args: [],
+        },
+        {
+            abi: vaultInterface,
+            address: vaultContractAddress,
+            method: "totalOutstandingDebt",
+            args: [],
+        },
+        {
+            abi: vaultInterface,
+            address: vaultContractAddress,
+            method: "getTotalAvailableCash",
+            args: [],
+        },
+        {
+            abi: vaultInterface,
+            address: vaultContractAddress,
+            method: "getCurrentExchangeRateInEther",
+            args: [],
+        },
+    ]);
+    const [
+        supplyRateResult,
+        totalOutstandingDebtResult,
+        totalAvailableCashResult,
+        currentExchangeRateInEtherResult,
+    ] = results;
+
+    console.log("DEBUG: supplyRateResult", supplyRateResult);
+    console.log(
+        "DEBUG: totalOutstandingDebtResult",
+        totalOutstandingDebtResult
+    );
+    console.log("DEBUG: totalAvailableCashResult", totalAvailableCashResult);
+
+    // Get supply APY
+    let supplyRatePerSecond = 0;
+    if (supplyRateResult) {
+        supplyRatePerSecond = supplyRateResult[0] / 1e18;
+    }
+    console.log("DEBUG: supplyRatePerSecond", supplyRatePerSecond);
+    const secondsPerDay = 86400;
+    const daysPerYear = 365;
+    const supplyAPY =
+        (Math.pow(supplyRatePerSecond * secondsPerDay + 1, daysPerYear) - 1) *
+        100;
+    console.log("DEBUG: supplyAPY", supplyAPY);
+    const APY = `${supplyAPY.toFixed(2)}%`;
+
+    // Get total TVL
+    let totalOutstandingDebt = 0;
+    if (totalOutstandingDebtResult) {
+        totalOutstandingDebt = totalOutstandingDebtResult[0] / 1e6;
+    }
+    let totalAvailableCash = 0;
+    if (totalAvailableCashResult) {
+        totalAvailableCash = totalAvailableCashResult[0] / 1e6;
+    }
+    console.log("DEBUG: totalOutstandingDebt", totalOutstandingDebt);
+    console.log("DEBUG: totalAvailableCash", totalAvailableCash);
+    const TVL = totalOutstandingDebt + totalAvailableCash;
+    console.log("DEBUG: TVL", TVL);
+    let dollarUSLocale = Intl.NumberFormat("en-US");
+
+    // Get user vault token balance
+    // TODO (bayu): Get decimals from contract
+    let balance = 0;
+    let balanceResult = useTokenBalance(vaultContractAddress, account);
+    console.log("DEBUG: balanceResult", balanceResult);
+    if (balanceResult) {
+        balance = balanceResult / 1e6; // TODO use decimals here from conttract
+    }
+    console.log("DEBUG: balance", balance);
+
+    // The the total value
+    let currentExchangeRate = 1.0;
+    if (currentExchangeRateInEtherResult) {
+        currentExchangeRate = currentExchangeRateInEtherResult[0] / 1e18;
+    }
+    console.log(
+        "DEBUG: currentExchangeRateInEtherResult",
+        currentExchangeRateInEtherResult
+    );
+    console.log("DEBUG: currentExchangeRate", currentExchangeRate);
+    const totalValueVaultToken = balance * currentExchangeRate;
+    console.log("DEBUG: totalValueVaultToken", totalValueVaultToken);
+
+    // Display the value
     const cardItems = [
         {
             title: "Your balance",
-            value: "0.00 rvUSDC",
-        },
-        {
-            title: "Available to withdraw",
-            value: "0.00 rvUSDC",
+            value: `${balance} rvUSDC`,
         },
         {
             title: "Total value",
-            value: "0.00 USDC",
+            value: `${totalValueVaultToken} USDC`,
         },
     ];
 
     return (
         <div>
             <Head>
-                <title>Earn 56.75% APY | Risedle Protocol</title>
+                <title>Earn {APY} APY | Risedle Protocol</title>
                 <meta
                     name="description"
                     content="Invest, earn and build on the decentralized crypto leveraged ETFs market protocol"
@@ -46,8 +143,8 @@ const Lend: NextPage = () => {
             <div className="mt-8 gap gap-y-8 flex flex-col">
                 <DetailHeader
                     image={USDC_ICON.src}
-                    title="Earn 56.75% APY"
-                    subTitle="TVL 10,000.00 USDC"
+                    title={`Earn ${APY} APY`}
+                    subTitle={`TVL ${dollarUSLocale.format(TVL)} USDC`}
                     leftTitle="Deposit"
                     leftPath="/lend/deposit"
                     rightTitle="Withdraw"
