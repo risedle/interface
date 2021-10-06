6c6ce3f2e9c37ed3aa3e4858f4bfa78aa7f495cc
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-10-06 16:44:11 +0700

    ❇️ Storybook: Add story for ExchangeForm approve and not approve

---
+++ stories/ExchangeFormApproved.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta, Story } from "@storybook/react";
+
+import ExchangeFormApproved from "../components/ExchangeFormAprroved";
+
+export default {
+    component: ExchangeFormApproved,
+    title: "Risedle/Exchange/ExchangeFormApproved",
+} as Meta;
+
+const Template: Story<{
+    backTitle: string;
+    backURL: string;
+    title: string;
+    subTitle: string;
+    formTitle: string;
+    formPlaceholder: string;
+    formInputToken: string;
+    formOutputToken: string;
+    formSubmitTitle: string;
+    onClickSubmit: () => void;
+}> = (args) => (
+    <ExchangeFormApproved
+        backTitle={args.backTitle}
+        backURL={args.backURL}
+        title={args.title}
+        subTitle={args.subTitle}
+        formTitle={args.formTitle}
+        formPlaceholder={args.formPlaceholder}
+        formInputToken={args.formInputToken}
+        formOutputToken={args.formOutputToken}
+        formSubmitTitle={args.formSubmitTitle}
+        onClickSubmit={args.onClickSubmit}
+    />
+);
+
+function submit() {
+    alert("submit");
+}
+
+//Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {
+    backTitle: "← Go back to lend",
+    backURL: "/lend",
+    title: "Deposit USDC",
+    subTitle: "Earn variable interest rate instantly.",
+    formTitle: "Deposit amount",
+    formPlaceholder: "Enter deposit amount",
+    formInputToken: "USDC",
+    formOutputToken: "rvUSDC",
+    formSubmitTitle: "Deposit",
+    onClickSubmit: submit,
+};
---
+++ stories/ExchangeFormNotApproved.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, Story } from "@storybook/react";
+
+import ExchangeFormNotApproved from "../components/ExchangeFormNotApproved";
+
+export default {
+    component: ExchangeFormNotApproved,
+    title: "Risedle/Exchange/ExchangeFormNotApproved",
+} as Meta;
+
+const Template: Story<{
+    backTitle: string;
+    backURL: string;
+    title: string;
+    subTitle: string;
+    formTitle: string;
+    formPlaceholder: string;
+    formInputToken: string;
+    formOutputToken: string;
+    onClickApprove: () => void;
+}> = (args) => (
+    <ExchangeFormNotApproved
+        backTitle={args.backTitle}
+        backURL={args.backURL}
+        title={args.title}
+        subTitle={args.subTitle}
+        formTitle={args.formTitle}
+        formPlaceholder={args.formPlaceholder}
+        formInputToken={args.formInputToken}
+        formOutputToken={args.formOutputToken}
+        onClickApprove={args.onClickApprove}
+    />
+);
+
+function approve() {
+    alert("approve");
+}
+
+//Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {
+    backTitle: "← Go back to lend",
+    backURL: "/lend",
+    title: "Deposit USDC",
+    subTitle: "Earn variable interest rate instantly.",
+    formTitle: "Deposit amount",
+    formPlaceholder: "Enter deposit amount",
+    formInputToken: "USDC",
+    formOutputToken: "rvUSDC",
+    onClickApprove: approve,
+};
