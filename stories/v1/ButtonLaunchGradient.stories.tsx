9679d3fe2bb6ceecea4d570af73e071561c87234
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-14 21:59:02 +0700

    ✦ Implement launch button gradient component

---
+++ stories/v1/ButtonLaunchGradient.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ButtonLaunchGradient from "../../components/v1/ButtonLaunchGradient";
+
+export default {
+    component: ButtonLaunchGradient,
+    title: "Risedle V1/Button/Launch Gradient",
+};
+
+export const Gradient = () => <ButtonLaunchGradient />;
