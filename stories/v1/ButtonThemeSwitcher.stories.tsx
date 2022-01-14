e8b944076fc5370f72a01a5eb31361d8237bc073
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-14 18:18:35 +0700

    ✦ Implement Theme Switcher button component

---
+++ stories/v1/ButtonThemeSwitcher.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ButtonThemeSwitcher from "../../components/v1/ButtonThemeSwitcher";
+
+export default {
+    component: ButtonThemeSwitcher,
+    title: "Risedle V1/Button/Theme Switcher",
+};
+
+export const Basic = () => <ButtonThemeSwitcher />;
