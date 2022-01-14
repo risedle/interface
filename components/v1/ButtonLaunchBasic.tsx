aea275b1ff59307a670e36e596075a810e93b607
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-14 21:17:07 +0700

    ✦ Implement Launch App button component

---
+++ components/v1/ButtonLaunchBasic.tsx
@@ -0,0 +1,24 @@
+import type { FunctionComponent } from "react";
+import Link from "next/link";
+
+/**
+ * ButtonLaunchBasicProps is a React Component properties that passed to React Component ButtonLaunchBasic
+ */
+type ButtonLaunchBasicProps = {};
+
+/**
+ * ButtonLaunchBasic is just yet another react component
+ *
+ * @link https://fettblog.eu/typescript-react/components/#functional-components
+ */
+const ButtonLaunchBasic: FunctionComponent<ButtonLaunchBasicProps> = ({}) => {
+    return (
+        <Link href="/app">
+            <a className="text-gray-light-12 dark:text-gray-dark-12 bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-5 dark:border-gray-dark-5 font-inter text-sm font-semibold py-3 px-4 rounded-full leading-none inline-block">
+                Launch App
+            </a>
+        </Link>
+    );
+};
+
+export default ButtonLaunchBasic;
---
+++ stories/v1/ButtonLaunchBasic.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ButtonLaunchBasic from "../../components/v1/ButtonLaunchBasic";
+
+export default {
+    component: ButtonLaunchBasic,
+    title: "Risedle V1/Button/Launch Basic",
+};
+
+export const Basic = () => <ButtonLaunchBasic />;
