b318c0e904bac374d00f3cf92801e1a5f8e7503d
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-15 16:45:14 +0700

    ✦ Implement FeatureCardLarge component

---
+++ stories/v1/FeatureCardLarge.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import FeatureCardLarge from "../../components/v1/FeatureCardLarge";
+
+export default {
+    component: FeatureCardLarge,
+    title: "Risedle V1/Home Page/Feature Card/Large",
+};
+
+export const Component = () => <FeatureCardLarge />;
