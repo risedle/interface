38f8111d21b6302a642e3015356f1a9ee5b95fec
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-17 19:32:53 +0700

    ✦ Implement ToastSuccess component

---
+++ stories/v1/Toasts/Success.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ToastSuccess from "../../../components/v1/Toasts/Success";
+
+export default {
+    component: ToastSuccess,
+    title: "Risedle V1/Toasts/Success",
+};
+
+export const Success = () => (
+    <ToastSuccess>Successfully minted 2.34 ETHRISE</ToastSuccess>
+);
