5d63a0677e270695fbbb4df7dc05b3e90f1516c8
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-01-17 22:19:31 +0700

    Add WalletConnectosrConfig
    This file contain the configured wallet connectors that will be used for connecting user's wallet to the app.

---
+++ config/WalletConnectorsConfig.tsx
@@ -0,0 +1,31 @@
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { chain } from 'wagmi'
+
+let kovanURL = "";
+if (process.env.NEXT_PUBLIC_KOVAN_URL) {
+    kovanURL = process.env.NEXT_PUBLIC_KOVAN_URL;
+}
+
+let arbitrumURL = "";
+if (process.env.NEXT_PUBLIC_ARBITRUM_URL) {
+    arbitrumURL = process.env.NEXT_PUBLIC_ARBITRUM_URL;
+}
+
+export const injected_connector = new InjectedConnector({
+    chains: [chain.kovan, chain.arbitrumOne]
+})
+
+export const getWCConnector = (_chainId: number) => {
+    return new WalletConnectConnector({
+        chains: [chain.kovan, chain.arbitrumOne],
+        options: {
+            qrcode: true,
+            chainId: _chainId,
+            rpc: {
+                [chain.kovan.id]: kovanURL,
+                [chain.arbitrumOne.id]: arbitrumURL,
+            }
+        }
+    })
+}
