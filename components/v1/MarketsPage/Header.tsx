191f33253301f4160b1da2f2b402f1456500d059
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-21 05:21:46 +0700

    ✦ Implement market page header

--- components/v1/MarketsPage/Header.tsx
+++ components/v1/MarketsPage/Header.tsx
@@ -4,6 +4,9 @@ import { useWalletContext } from "../Wallet";
 // Snapshot data
 import { useMarkets } from "../../../utils/snapshot";
 
+// Formatters
+import { dollarFormatter } from "../../../utils/formatters";
+
 /**
  * HeaderProps is a React Component properties that passed to React Component Header
  */
@@ -16,32 +19,26 @@ type HeaderProps = {};
  */
 const Header: FunctionComponent<HeaderProps> = ({}) => {
     const { chain } = useWalletContext();
-    const { markets, isLoading, isError } = useMarkets(chain.id);
-    console.debug("Header markets", markets);
-    console.debug("Header isLoading", isLoading);
-    console.debug("Header isError", isError);
+    const { markets, isLoading } = useMarkets(chain.id);
 
     // Get total AUM
     // Total collateral all of leveraged tokens in terms of USDC
 
     return (
-        <div className="flex flex-col px-4 space-y-10">
+        <div className="flex flex-col px-4 space-y-6">
             <div className="text-center flex flex-col space-y-2">
-                <div>
-                    <h1 className="m-0 font-bold tracking-tighter text-4xl leading-8 text-gray-light-12 dark:text-gray-dark-12">Leveraged Token</h1>
-                </div>
-                <div>
-                    <h1 className="m-0 font-bold tracking-tighter text-4xl leading-8 text-gray-light-12 dark:text-gray-dark-12">Market</h1>
-                </div>
+                <h1 className="m-0 font-bold tracking-tighter text-2xl leading-8 text-gray-light-12 dark:text-gray-dark-12">Leveraged Token Market</h1>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row space-x-6 pb-4 border-b border-gray-light-9 dark:border-gray-dark-9 border-dashed">
                 <div className="basis-1/2 text-center flex flex-col space-y-2">
-                    <p className="text-sm leading-4 text-gray-light-10 dark:text-gray-dark-10">Total AUM</p>
-                    <p className="font-ibm font-semibold text-sm leading-4 tracking-tighter text-gray-light-12 dark:text-gray-dark-12">$36,823.378</p>
+                    <p className="text-sm leading-4 text-gray-light-10 dark:text-gray-dark-10">AUM</p>
+                    {isLoading && <div className="h-4 bg-gray-light-3 dark:bg-gray-dark-3 rounded-full animate-pulse"></div>}
+                    {!isLoading && <p className="font-ibm font-semibold text-sm leading-4 tracking-tighter text-gray-light-12 dark:text-gray-dark-12">{dollarFormatter.format(markets.aum)}</p>}
                 </div>
                 <div className="basis-1/2 text-center flex flex-col space-y-2">
-                    <p className="text-sm leading-4 text-gray-light-10 dark:text-gray-dark-10">Total Value Locked</p>
-                    <p className="font-ibm font-semibold text-sm leading-4 tracking-tighter text-gray-light-12 dark:text-gray-dark-12">$36,823.378</p>
+                    <p className="text-sm leading-4 text-gray-light-10 dark:text-gray-dark-10">TVL</p>
+                    {isLoading && <div className="h-4 bg-gray-light-3 dark:bg-gray-dark-3 rounded-full animate-pulse"></div>}
+                    {!isLoading && <p className="font-ibm font-semibold text-sm leading-4 tracking-tighter text-gray-light-12 dark:text-gray-dark-12">{dollarFormatter.format(markets.tvl)}</p>}
                 </div>
             </div>
         </div>
