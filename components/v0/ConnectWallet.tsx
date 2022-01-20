235a2549698cc6aabbbed67554aaa98c5bf6ee1b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-21 03:42:25 +0700

    ✦ Remove all unused pages

--- components/v0/ConnectWallet.tsx
+++ components/v0/ConnectWallet.tsx
@@ -8,9 +8,6 @@ import Link from "next/link";
 // Import wagmi
 import { useAccount } from "wagmi";
 
-// Import wagmi
-import { useAccount } from "wagmi";
-
 // Import button
 import ButtonOutline from "./ButtonOutline";
 import ButtonBlueSecondary from "./ButtonBlueSecondary";
@@ -27,22 +24,13 @@ type ConnectWalletProps = {};
  * @link https://fettblog.eu/typescript-react/components/#functional-components
  */
 const ConnectWallet: FunctionComponent<ConnectWalletProps> = ({}) => {
-<<<<<<< HEAD:components/v0/ConnectWallet.tsx
     const [accountData, disconnect] = useAccount();
-=======
-
-    const [accountData, disconnect] = useAccount()
->>>>>>> 71d1a8d466188ac73be7b9b43b54add4ecb4464b:components/ConnectWallet.tsx
 
     let isAccountConnected = false;
     let shortAccountAddress = "NOT_CONNECTED";
     if (accountData.data?.address) {
         isAccountConnected = true;
-<<<<<<< HEAD:components/v0/ConnectWallet.tsx
         // shortAccountAddress = shortenAddress(accountData.data.address);
-=======
-        shortAccountAddress = shortenAddress(accountData.data.address);
->>>>>>> 71d1a8d466188ac73be7b9b43b54add4ecb4464b:components/ConnectWallet.tsx
     }
 
     if (isAccountConnected) {
@@ -52,30 +40,16 @@ const ConnectWallet: FunctionComponent<ConnectWalletProps> = ({}) => {
                     <ButtonBlueSecondary>{shortAccountAddress}</ButtonBlueSecondary>
                 </div>
                 <div>
-<<<<<<< HEAD:components/v0/ConnectWallet.tsx
                     <ButtonOutline onClick={disconnect}>Disconnect</ButtonOutline>
-=======
-                    <ButtonOutline onClick={disconnect}>
-                        Disconnect
-                    </ButtonOutline>
->>>>>>> 71d1a8d466188ac73be7b9b43b54add4ecb4464b:components/ConnectWallet.tsx
                 </div>
             </div>
         );
     } else {
         return (
             <Fragment>
-<<<<<<< HEAD:components/v0/ConnectWallet.tsx
                 <Link href={"/connect"}>
                     <a>
                         <ButtonOutline>Connect wallet</ButtonOutline>
-=======
-                <Link href={'/connect'}>
-                    <a>
-                        <ButtonOutline>
-                            Connect wallet
-                        </ButtonOutline>
->>>>>>> 71d1a8d466188ac73be7b9b43b54add4ecb4464b:components/ConnectWallet.tsx
                     </a>
                 </Link>
             </Fragment>
@@ -83,8 +57,4 @@ const ConnectWallet: FunctionComponent<ConnectWalletProps> = ({}) => {
     }
 };
 
-<<<<<<< HEAD:components/v0/ConnectWallet.tsx
 export default ConnectWallet;
-=======
-export default ConnectWallet;
->>>>>>> 71d1a8d466188ac73be7b9b43b54add4ecb4464b:components/ConnectWallet.tsx
