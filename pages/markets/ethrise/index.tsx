8dd5b70c76ca22971cc49bac8e2d0fa7efb1e851
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-24 04:03:05 +0700

    ✦ Enable ETHRISE page market

---
+++ pages/markets/ethrise/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+// Import components
+import ETHRISEPage from "../../../components/v1/MarketDetailPage/ETHRISEPage";
+
+const ETHRISE: NextPage = () => {
+    return <ETHRISEPage />;
+};
+
+export default ETHRISE;
