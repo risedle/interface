e06a84d841ebb2709690ad40ff7ce1a9480dbabc
pyk.eth ve(✦,✦) <bayualdiyansyah@gmail.com>
2022-01-25 22:41:56 +0700

    ✦ Extract background gradient as component (#44)

---
+++ stories/v1/MarketDetailPage/BackgroundGradient.stories.tsx
@@ -0,0 +1,8 @@
+import BackgroundGradient from "../../../components/v1/MarketDetailPage/BackgroundGradient";
+
+export default {
+    component: BackgroundGradient,
+    title: "Risedle V1/Market Detail Page/Background Gradient Component",
+};
+
+export const BackgroundGradientComponent = () => <BackgroundGradient />;
