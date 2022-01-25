b161b2440b4782bd2fa636f2b7301b9a7454e3ad
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-25 23:25:07 +0700

    ✦ Minor dialog improvement on desktop

--- components/v1/Buttons/ConnectWalletDesktop.tsx
+++ components/v1/Buttons/ConnectWalletDesktop.tsx
@@ -11,6 +11,7 @@ import ToastError from "../Toasts/Error";
 import ToastSuccess from "../Toasts/Success";
 // States
 import { MetaMaskConnector, useWalletContext, WCConnector } from "../Wallet";
+import ButtonClose from "./Close";
 
 /**
  * ButtonConnectWalletDesktopProps is a React Component properties that passed to React Component ButtonConnectWalletDesktop
@@ -39,7 +40,7 @@ const getEtherscanAddressURL = (chain: Chain | null, address: string): string =>
  */
 const ButtonConnectWalletDesktop: FunctionComponent<ButtonConnectWalletDesktopProps> = ({}) => {
     const { account, login, logout, connectorName, setConnectorName, chain } = useWalletContext();
-    const [connectionData, connect] = useConnect();
+    const [, connect] = useConnect();
     const [connectedChain, switchNetwork] = useNetwork();
     const [accountData] = useAccount();
     let [isOpen, setIsOpen] = useState(false);
@@ -111,22 +112,18 @@ const ButtonConnectWalletDesktop: FunctionComponent<ButtonConnectWalletDesktopPr
 
     return (
         <>
-            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed z-10 inset-0 overflow-y-auto">
+            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed z-20 inset-0 overflow-y-auto">
                 <div className="flex items-center justify-center min-h-screen">
                     <Dialog.Overlay className="fixed inset-0 bg-gray-dark-1/60 dark:bg-black/60 backdrop-blur" />
 
                     <div className="flex flex-col relative bg-gray-light-1 dark:bg-gray-dark-1 border border-gray-light-3 dark:border-gray-dark-3 rounded-[24px] max-w-sm mx-auto w-[342px]">
-                        <Dialog.Title className="text-center pr-4 py-4 pl-[49px] border-b border-gray-light-3 dark:border-gray-dark-3 border-dashed m-0">
-                            <span className="text-base leading-none font-bold text-gray-light-12 dark:text-gray-dark-12">{isConnecting ? "Connecting..." : "Connect Wallet"}</span>
-                            <button className="float-right bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-4 dark:border-gray-dark-4 rounded-full align-middle self-center" onClick={() => setIsOpen(false)}>
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="align-middle inline-block fill-gray-light-12 dark:fill-gray-dark-12 m-2">
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
-                                    />
-                                </svg>
-                            </button>
+                        <Dialog.Title className="flex flex-row items-center px-4 py-4 justify-between border-b border-gray-light-3 dark:border-gray-dark-3 border-dashed m-0">
+                            <span className="grow text-center pl-[40px] text-base leading-none font-bold text-gray-light-12 dark:text-gray-dark-12">{isConnecting ? "Connecting..." : "Connect Wallet"}</span>
+                            <ButtonClose
+                                onClick={() => {
+                                    setIsOpen(false);
+                                }}
+                            />
                         </Dialog.Title>
 
                         <div className="flex flex-col p-4 space-y-2">
--- components/v1/Buttons/NetworkSwitcher.tsx
+++ components/v1/Buttons/NetworkSwitcher.tsx
@@ -7,6 +7,7 @@ import { chain as Chains, Chain, useNetwork } from "wagmi";
 import { useWalletContext, supportedChains } from "../Wallet";
 
 import RisedleLinks from "../../../utils/links";
+import ButtonClose from "./Close";
 
 /**
  * ButtonNetworkSwitcherProps is a React Component properties that passed to React Component ButtonNetworkSwitcher
@@ -49,36 +50,32 @@ const ButtonNetworkSwitcher: FunctionComponent<ButtonNetworkSwitcherProps> = ({}
 
     return (
         <>
-            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed z-10 inset-0 overflow-y-auto">
+            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed z-20 inset-0 overflow-y-auto">
                 <div className="flex items-center justify-center min-h-screen">
-                    <Dialog.Overlay className="fixed inset-0 bg-white/20 dark:bg-black/20 backdrop-blur" />
+                    <Dialog.Overlay className="fixed inset-0 bg-gray-dark-1/60 dark:bg-black/60 backdrop-blur" />
 
-                    <div className="flex flex-col relative bg-gray-light-1 dark:bg-gray-dark-1 border border-gray-light-3 dark:border-gray-dark-3 rounded-[24px] max-w-sm mx-auto">
-                        <Dialog.Title className="text-center pr-4 py-4 pl-[49px] border-b border-gray-light-3 dark:border-gray-dark-3 border-dashed m-0">
-                            <span className="text-base leading-none font-bold text-gray-light-12 dark:text-gray-dark-12">Switch a Network</span>
-                            <button className="float-right bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-4 dark:border-gray-dark-4 rounded-full align-middle self-center" onClick={() => setIsOpen(false)}>
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="align-middle inline-block fill-gray-light-12 dark:fill-gray-dark-12 m-2">
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
-                                    />
-                                </svg>
-                            </button>
+                    <div className="flex flex-col relative bg-gray-light-1 dark:bg-gray-dark-1 border border-gray-light-3 dark:border-gray-dark-3 rounded-[24px] w-[342px] mx-auto">
+                        <Dialog.Title className="flex flex-row items-center px-4 py-4 justify-between border-b border-gray-light-3 dark:border-gray-dark-3 border-dashed m-0">
+                            <span className="grow text-center pl-[40px] text-base leading-none font-bold text-gray-light-12 dark:text-gray-dark-12">Switch a Network</span>
+                            <ButtonClose
+                                onClick={() => {
+                                    setIsOpen(false);
+                                }}
+                            />
                         </Dialog.Title>
 
                         <div className="flex flex-col max-w-[342px] p-4 space-y-2">
                             {supportedChains.map((c) => {
                                 return (
                                     <button
-                                        className="bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-5 dark:border-gray-dark-5 rounded-[12px] py-[11px] px-[12px] text-left w-full m-0"
+                                        className="flex flex-row space-x-4 items-center bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-5 dark:border-gray-dark-5 rounded-[12px] py-[11px] px-[11px] text-left w-full m-0"
                                         onClick={() => {
                                             switchToNetwork(c);
                                         }}
                                         key={c.id}
                                     >
-                                        <img src={getChainIconPath(c)} alt={c.name} className="inline-block self-center bg-gray-light-4 dark:bg-gray-dark-4 p-[6px] rounded-full mr-4" />
-                                        <span className="text-sm text-gray-light-12 dark:text-gray-dark-12 font-semibold font-inter m-0 leading-none">{c.name}</span>
+                                        <img src={getChainIconPath(c)} alt={c.name} className="w-[32px] h-[32px]" />
+                                        <span className="text-sm leading-4 text-gray-light-12 dark:text-gray-dark-12 font-semibold tracking-[-0.02em]">{c.name}</span>
                                     </button>
                                 );
                             })}
