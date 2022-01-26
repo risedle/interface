a072f5d988e9409526c59dc55b45904f8a7f7b45
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-01-26 12:53:50 +0700

    ✦ Implement Button Components (#49)
    * Add ButtonPrimary Component
    
    Done:
    Implement hover and pressed state
    Todo:
    Implement loading and disabled state
    
    * Add Button Secondary Component
    
    Done:
    Implement hover and pressed state
    Todo:
    Implement loading and disabled state
    
    * Implement ButtonPrimary in ETHRISEPage
    
    * Implement ButtonPositive Component
    
    * Implement ButtonNegative Component
    
    * Update Tailwind Colors Configuration
    
    * Fix Deposit or Withdraw Button
    
    * Revert "Implement ButtonPrimary in ETHRISEPage"
    
    This reverts commit a86a9718111d83ef0116dcbeca0bb789d40aeaa3.
    
    * Revert "Fix Deposit or Withdraw Button"
    
    This reverts commit 46f66958c774bdfe9d4f1eefd573bba4295d23ce.

---
+++ stories/v1/Buttons/ButtonSecondary.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ButtonSecondary from "../../../components/v1/Buttons/ButtonSecondary";
+
+export default {
+    component: ButtonSecondary,
+    title: "Risedle V1/Buttons/Secondary",
+};
+
+export const Secondary = () => {
+    return(
+        <ButtonSecondary>
+            Label
+        </ButtonSecondary>
+    )
+}
