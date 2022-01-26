4909a13f5e3bd4c81e6ae5da34de14ae16b99377
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-27 03:55:03 +0700

    ✦ Optimize minting process

--- components/v1/MarketMetadata.ts
+++ components/v1/MarketMetadata.ts
@@ -4,6 +4,7 @@ import { chain as Chains } from "wagmi";
 export type Metadata = {
     logo: string;
     vaultLogo: string;
+    vaultAddress: string;
     title: string;
     subtitle: string;
     path: string;
@@ -12,12 +13,12 @@ export type Metadata = {
     vaultInformationText: string;
     collateralSymbol: string;
     collateralDecimals: number;
-    collateralContract: string;
+    collateralAddress: string;
     debtSymbol: string;
     debtDecimals: number;
-    debtContract: string;
+    debtAddress: string;
+    oracleAddress: string;
     uniswapSwapURL: string;
-    oracleContract: string;
 };
 export type MarketMetadata = Record<string, Metadata>;
 export type MarketMetadataRecord = Record<number, MarketMetadata>;
@@ -28,6 +29,7 @@ export const Metadata: MarketMetadataRecord = {
             subtitle: "ETH Leverage Market",
             logo: "/markets/ethrise.svg",
             vaultLogo: "/markets/usdc.svg",
+            vaultAddress: "0x42B6BAE111D9300E19F266Abf58cA215f714432c",
             path: "/markets/ethrise",
             description:
                 "Enjoy leveraged ETH without risk of liquidation or earn yield from your idle USDC",
@@ -37,12 +39,12 @@ export const Metadata: MarketMetadataRecord = {
                 "rvETHUSDC is an interest-bearing token that increase value overtime. Start earning variable interest rate in real time by depositing USDC.",
             collateralSymbol: "ETH",
             collateralDecimals: 18, // ETH is 18 decimals
-            collateralContract: "0x4470E84Ce5C1fe89EA35a19560b6f2b1Ed71507f",
+            collateralAddress: "0x4470E84Ce5C1fe89EA35a19560b6f2b1Ed71507f",
             debtSymbol: "USDC",
             debtDecimals: 6, // USDC is 6 decimals
-            debtContract: "0x0af08696cb51e81456dc0a1dee7f8bfad8d82a22",
+            debtAddress: "0x0af08696cb51e81456dc0a1dee7f8bfad8d82a22",
             uniswapSwapURL: "#",
-            oracleContract: "0x1F6Ec9B472b5EB3c7aA617Ce45ea2ed4f1A2db7D",
+            oracleAddress: "0x1F6Ec9B472b5EB3c7aA617Ce45ea2ed4f1A2db7D",
         },
     },
 };
