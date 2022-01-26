db887a1e2a7f0d76b2fe2811105c93b4a3bb770f
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-27 00:38:58 +0700

    ✦ Provide link to tx on the minting button

--- components/v1/MarketDetailPage/ETHRISEPage.tsx
+++ components/v1/MarketDetailPage/ETHRISEPage.tsx
@@ -157,6 +157,7 @@ const ETHRISEPage: FunctionComponent<ETHRISEPageProps> = ({}) => {
 
     // Action states
     const [isMinting, setIsMinting] = useState(false);
+    const [mintingHash, setMingtingHash] = useState<string | undefined>(undefined);
 
     // Set initial data for onMouseLeave event on the price chart
     if (initialNAV === 0 && initialNAVChange === 0 && initialSupplyAPY === 0 && initialBorrowAPY === 0 && market) {
@@ -678,17 +679,18 @@ const ETHRISEPage: FunctionComponent<ETHRISEPageProps> = ({}) => {
                                                                                                     return;
                                                                                                 }
 
-                                                                                                const it = toast.custom((t) => <ToastTransaction hash={result.data.hash} />, { duration: Infinity });
-                                                                                                const receipt = await result.data.wait(3);
-                                                                                                toast.dismiss(it);
+                                                                                                setMingtingHash(result.data.hash);
+                                                                                                toast.custom((t) => <ToastTransaction hash={result.data.hash} />);
+                                                                                                const receipt = await result.data.wait();
                                                                                                 setIsMinting(false);
+                                                                                                setMingtingHash(undefined);
                                                                                                 if (receipt.status === 1) {
                                                                                                     // success
                                                                                                     const event = receipt.logs[receipt.logs.length - 1];
                                                                                                     const confirmedMintedAmount = ethers.utils.formatUnits(event.data, collateralDecimals);
                                                                                                     toast.custom((t) => (
                                                                                                         <ToastSuccess>
-                                                                                                            Successfully minted {parseFloat(confirmedMintedAmount).toFixed(3)} {title}{" "}
+                                                                                                            Successfully minted {parseFloat(confirmedMintedAmount).toFixed(3)} {title}
                                                                                                         </ToastSuccess>
                                                                                                     ));
                                                                                                 } else {
@@ -700,7 +702,7 @@ const ETHRISEPage: FunctionComponent<ETHRISEPageProps> = ({}) => {
                                                                                             Mint
                                                                                         </button>
                                                                                     )}
-                                                                                    {isMinting && (
+                                                                                    {isMinting && !mintingHash && (
                                                                                         <button className="cursor-wait flex flex-row text-center justify-center space-x-2 bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-4 dark:border-gray-dark-4 text-blue-dark-1 dark:text-blue-light-1 text-sm leading-4 font-semibold py-[11px] px-4 rounded-full leading-4 inline-block tracking-[-0.02em] w-full drop-shadow-[0_0_45px_rgba(54,158,255,0.1)]">
                                                                                             <svg width="17" height="16" viewBox="0 0 17 16" className="fill-gray-light-12 dark:fill-gray-dark-12 animate-spin" xmlns="http://www.w3.org/2000/svg">
                                                                                                 <g clipPath="url(#clip0_1161_30088)">
@@ -710,6 +712,21 @@ const ETHRISEPage: FunctionComponent<ETHRISEPageProps> = ({}) => {
                                                                                             </svg>
                                                                                             <div>Minting...</div>
                                                                                         </button>
+                                                                                    )}
+                                                                                    {isMinting && mintingHash && chain.blockExplorers && chain.blockExplorers.length && (
+                                                                                        <Link href={`${chain.blockExplorers[0].url}/tx/${mintingHash}`}>
+                                                                                            <a target="_blank" rel="noreferrer" className="flex flex-row text-center justify-center space-x-2 bg-gray-light-2 dark:bg-gray-dark-2 border border-gray-light-4 dark:border-gray-dark-4 text-blue-dark-1 dark:text-blue-light-1 text-sm leading-4 font-semibold py-[11px] px-4 rounded-full leading-4 inline-block tracking-[-0.02em] w-full drop-shadow-[0_0_45px_rgba(54,158,255,0.1)]">
+                                                                                                <svg width="17" height="16" viewBox="0 0 17 16" className="fill-gray-light-12 dark:fill-gray-dark-12 animate-spin" xmlns="http://www.w3.org/2000/svg">
+                                                                                                    <g clipPath="url(#clip0_1161_30088)">
+                                                                                                        <path opacity="0.2" d="M16.5 8.00049C16.5 12.4188 12.9183 16.0005 8.5 16.0005C4.08172 16.0005 0.5 12.4188 0.5 8.00049C0.5 3.58221 4.08172 0.000488281 8.5 0.000488281C12.9183 0.000488281 16.5 3.58221 16.5 8.00049ZM2.9 8.00049C2.9 11.0933 5.40721 13.6005 8.5 13.6005C11.5928 13.6005 14.1 11.0933 14.1 8.00049C14.1 4.90769 11.5928 2.40049 8.5 2.40049C5.40721 2.40049 2.9 4.90769 2.9 8.00049Z" />
+                                                                                                        <path d="M15.3 8.00049C15.9627 8.00049 16.5092 8.5407 16.4102 9.196C16.3136 9.83526 16.1396 10.4619 15.891 11.062C15.489 12.0326 14.8997 12.9145 14.1569 13.6573C13.414 14.4002 12.5321 14.9895 11.5615 15.3915C10.9614 15.6401 10.3348 15.814 9.69551 15.9107C9.04021 16.0097 8.5 15.4632 8.5 14.8005C8.5 14.1377 9.04326 13.6133 9.69084 13.4724C10.0157 13.4017 10.3344 13.3021 10.643 13.1742C11.3224 12.8928 11.9398 12.4803 12.4598 11.9603C12.9798 11.4403 13.3923 10.8229 13.6737 10.1435C13.8016 9.83489 13.9012 9.51619 13.9719 9.19133C14.1129 8.54375 14.6373 8.00049 15.3 8.00049Z" />
+                                                                                                    </g>
+                                                                                                </svg>
+                                                                                                <div>
+                                                                                                    Minting {mintingHash.slice(0, 6)}...{mintingHash.slice(-4)} &#8599;
+                                                                                                </div>
+                                                                                            </a>
+                                                                                        </Link>
                                                                                     )}
                                                                                 </div>
                                                                             </div>
@@ -1000,7 +1017,9 @@ const ETHRISEPage: FunctionComponent<ETHRISEPageProps> = ({}) => {
                 <ButtonConnectWalletMobile />
             </div>
 
-            <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
+            <div className="sm:hidden">
+                <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
+            </div>
         </>
     );
 };
