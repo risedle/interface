1b4ead6619c676c9345ca79edc3623f08bffee06
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-01-26 11:44:44 +0700

    Add ButtonPrimary Component
    Done:
    Implement hover and pressed state
    Todo:
    Implement loading and disabled state

---
+++ stories/v1/Buttons/ButtonPrimary.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ButtonPrimary from "../../../components/v1/Buttons/ButtonPrimary";
+
+export default {
+    component: ButtonPrimary,
+    title: "Risedle V1/Buttons/Primary",
+};
+
+export const Primary = () => {
+    return(
+        <ButtonPrimary>
+            Label
+        </ButtonPrimary>
+    )
+}
