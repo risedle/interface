81c42bbb31e17a1799872d93c060bec28dc74450
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-01-26 12:08:59 +0700

    Implement ButtonPositive Component

---
+++ stories/v1/Buttons/ButtonPositive.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ButtonPositive from "../../../components/v1/Buttons/ButtonPositive";
+
+export default {
+    component: ButtonPositive,
+    title: "Risedle V1/Buttons/Positive",
+};
+
+export const Positive = () => {
+    return(
+        <ButtonPositive>
+            Label
+        </ButtonPositive>
+    )
+}
