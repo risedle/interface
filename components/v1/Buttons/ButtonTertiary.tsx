a51afb0b4bd8b049ce23a52e1a5e7c049e22e6d2
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-01-27 20:20:04 +0700

    Add ButtonTertiary Component

---
+++ components/v1/Buttons/ButtonTertiary.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+
+/**
+ * ButtonTertiaryProps is a React Component properties that passed to React Component ButtonTertiary
+ */
+type ButtonTertiaryProps = {
+    onClick?: () => void;
+    full?: boolean; // if full is true, then the button will have w-full class
+};
+
+/**
+ * ButtonTertiary is just yet another react component
+ *
+ * @link https://fettblog.eu/typescript-react/components/#functional-components
+ */
+
+const ButtonTertiary: FunctionComponent<ButtonTertiaryProps> = ({
+    onClick,
+    full,
+    children
+}) => {
+    return(
+        <button className={`button basic ${full ? 'w-full' : ''}`} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+export default ButtonTertiary;
