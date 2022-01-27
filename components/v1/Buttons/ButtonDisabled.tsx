38154487c7a37a9b6dbfe17ae9b029ea10f33213
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-01-27 20:19:24 +0700

    Add Button Class to Button Components

--- components/v1/Buttons/ButtonDisabled.tsx
+++ components/v1/Buttons/ButtonDisabled.tsx
@@ -19,7 +19,7 @@ const ButtonDisabled: FunctionComponent<ButtonDisabledProps> = ({
     children
 }) => {
     return(
-        <button disabled className={`cursor-not-allowed bg-gray-light-4 dark:bg-gray-dark-4 border border-gray-light-5 dark:border-gray-dark-5 rounded-full text-sm leading-4 tracking-[-0.02em] text-gray-light-10 dark:text-gray-dark-10 font-semibold py-[12px] px-[24px] transition ease-out duration-300 ${full ? 'w-full' : ''}`}>
+        <button disabled className={`button cursor-not-allowed bg-gray-light-4 dark:bg-gray-dark-4 border border-gray-light-5 dark:border-gray-dark-5 rounded-full text-sm leading-4 tracking-[-0.02em] text-gray-light-10 dark:text-gray-dark-10 font-semibold py-[12px] px-[24px] transition ease-out duration-300 ${full ? 'w-full' : ''}`}>
             {children}
         </button>
     )
--- components/v1/Buttons/ButtonSecondary.tsx
+++ components/v1/Buttons/ButtonSecondary.tsx
@@ -21,7 +21,7 @@ const ButtonSecondary: FunctionComponent<ButtonSecondaryProps> = ({
     children
 }) => {
     return(
-        <button className={`bg-blue-light-2 dark:bg-blue-dark-2 border border-blue-light-5 dark:border-blue-dark-5 rounded-full text-sm leading-4 tracking-[-0.02em] text-blue-light-11 dark:text-blue-dark-11 font-semibold py-[12px] px-[24px] transition ease-out duration-300 hover:bg-blue-light-3 hover:dark:bg-blue-dark-3 hover:border-blue-light-6 hover:dark:border-blue-dark-6 active:bg-blue-light-4 active:dark:bg-blue-dark-4 active:border-blue-light-7 active:dark:border-blue-dark-7 ${full ? 'w-full' : ''}`} onClick={onClick}>
+        <button className={`button bg-blue-light-2 dark:bg-blue-dark-2 border border-blue-light-5 dark:border-blue-dark-5 rounded-full text-sm leading-4 tracking-[-0.02em] text-blue-light-11 dark:text-blue-dark-11 font-semibold py-[12px] px-[24px] transition ease-out duration-300 hover:bg-blue-light-3 hover:dark:bg-blue-dark-3 hover:border-blue-light-6 hover:dark:border-blue-dark-6 active:bg-blue-light-4 active:dark:bg-blue-dark-4 active:border-blue-light-7 active:dark:border-blue-dark-7 ${full ? 'w-full' : ''}`} onClick={onClick}>
             {children}
         </button>
     )
