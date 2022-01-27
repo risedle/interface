060569c97b7c0d69f202ed6d66c99d29e16e915c
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-01-27 19:50:22 +0700

    Add ButtonLoading Component

---
+++ stories/v1/Buttons/ButtonLoading.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ButtonLoading from "../../../components/v1/Buttons/ButtonLoading";
+
+export default {
+    component: ButtonLoading,
+    title: "Risedle V1/Buttons/Loading",
+};
+
+export const Loading = () => {
+    return(
+        <ButtonLoading>
+            Label...
+        </ButtonLoading>
+    )
+}
