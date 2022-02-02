c9b52f193a0ae2010caf1402237751036b621e75
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-02-03 02:28:12 +0700

    ✦ Fix formatting

--- abis/OracleABI.ts
+++ abis/OracleABI.ts
@@ -1,7 +1,5 @@
 import { ethers } from "ethers";
 
-export const OracleABI = new ethers.utils.Interface([
-    "function getPrice() external view returns (uint256 price)",
-]);
+export const OracleABI = new ethers.utils.Interface(["function getPrice() external view returns (uint256 price)"]);
 
 export default OracleABI;
--- components/v1/MarketDetailPage/Explorer.ts
+++ components/v1/MarketDetailPage/Explorer.ts
@@ -1,7 +1,5 @@
 import { Chain } from "wagmi";
 
 export const getExplorerLink = (chain: Chain, hash: string) => {
-    return chain.blockExplorers && chain.blockExplorers.length
-        ? `${chain.blockExplorers[0].url}/tx/${hash}`
-        : "#";
+    return chain.blockExplorers && chain.blockExplorers.length ? `${chain.blockExplorers[0].url}/tx/${hash}` : "#";
 };
