e183eaa01e35cfdbe5fc8e5dc426248b3b1512d9
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-02-02 13:34:06 +0700

    Revert "Add Specific WCConnector for Each Network"
    This reverts commit 766c7f9df1d35d47ef1a8b4fd6e1a15c5932f1f5.

--- components/v1/Wallet.tsx
+++ components/v1/Wallet.tsx
@@ -5,7 +5,6 @@ import { createContext, useContext } from "react";
 import { ethers, providers } from "ethers";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { getWCConnector } from "../../config/WalletConnectorsConfig";
 
 export const connectorStorageKey = "risedleConnectors.wallet";
 
@@ -17,19 +16,10 @@ export const MetaMaskConnector = new InjectedConnector({
     chains: supportedChains,
 });
 
-export const WCConnectorArbitrum = new WalletConnectConnector({
+export const WCConnector = new WalletConnectConnector({
     chains: supportedChains,
     options: {
         qrcode: true,
-        chainId: Chains.arbitrumOne.id
-    },
-});
-
-export const WCConnectorKovan = new WalletConnectConnector({
-    chains: supportedChains,
-    options: {
-        qrcode: true,
-        chainId: Chains.kovan.id
     },
 });
 
@@ -134,7 +124,7 @@ export const Wallet: FunctionComponent<WalletProps> = ({ children }) => {
 
     return (
         <WalletContext.Provider value={sharedPersistentStates}>
-            <Provider autoConnect={true} connectorStorageKey={connectorStorageKey} connectors={[MetaMaskConnector, WCConnectorArbitrum, WCConnectorKovan]} provider={getProvider}>
+            <Provider autoConnect={true} connectorStorageKey={connectorStorageKey} connectors={[MetaMaskConnector, WCConnector]} provider={getProvider}>
                 {children}
             </Provider>
         </WalletContext.Provider>
