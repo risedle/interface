6b488460dfab7f0aa8f125e099556da8daab2f3a
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-02-04 20:39:06 +0700

    ✦ DepositOrWithdraw use global states

--- components/v1/MarketDetailPage/ButtonDepositOrWithdraw.tsx
+++ components/v1/MarketDetailPage/ButtonDepositOrWithdraw.tsx
@@ -2,7 +2,7 @@ import type { FunctionComponent } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import * as Tabs from "@radix-ui/react-tabs";
 import { Metadata } from "../MarketMetadata";
-import { useWalletContext } from "../Wallet";
+import { DEFAULT_CHAIN, useWalletContext } from "../Wallet";
 import Deposit from "./Deposit";
 import Withdraw from "./Withdraw";
 
@@ -20,7 +20,8 @@ type ButtonDepositOrWithdrawProps = {
  */
 const ButtonDepositOrWithdraw: FunctionComponent<ButtonDepositOrWithdrawProps> = ({ address }) => {
     const { chain } = useWalletContext();
-    const metadata = Metadata[chain.id][address];
+    const chainID = chain.unsupported ? DEFAULT_CHAIN.id : chain.chain.id;
+    const metadata = Metadata[chainID][address];
 
     return (
         <Dialog.Root>
