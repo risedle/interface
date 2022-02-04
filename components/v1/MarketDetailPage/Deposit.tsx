00620403032c8c674271a0ad3f5b7abbf08d87bc
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-02-04 20:39:36 +0700

    ✦ Deposit: refactor approval process

--- components/v1/MarketDetailPage/Deposit.tsx
+++ components/v1/MarketDetailPage/Deposit.tsx
@@ -1,10 +1,10 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useState } from "react";
 import Link from "next/link";
 import { ethers } from "ethers";
-import { erc20ABI, useProvider, useSigner } from "wagmi";
+import { erc20ABI, useContractWrite } from "wagmi";
 import toast from "react-hot-toast";
 
-import { useWalletContext } from "../Wallet";
+import { DEFAULT_CHAIN, useWalletContext } from "../Wallet";
 import { Metadata } from "../MarketMetadata";
 import ToastError from "../Toasts/Error";
 import ToastSuccess from "../Toasts/Success";
@@ -13,10 +13,12 @@ import FormLoadingFailed from "./FormLoadingFailed";
 import ButtonPositive from "../Buttons/ButtonPositive";
 import ButtonLoading from "../Buttons/ButtonLoading";
 
-import { RequestState, ApprovalState } from "./States";
+import { ApprovalState } from "./States";
 import { getExplorerLink } from "./Explorer";
 import DepositForm from "./DepositForm";
 import ToastInProgress from "../Toasts/InProgress";
+import { useTokenAllowance } from "../swr/useTokenAllowance";
+import { useTokenBalance } from "../swr/useTokenBalance";
 
 /**
  * DepositProps is a React Component properties that passed to React Component Deposit
@@ -32,38 +34,39 @@ type DepositProps = {
  */
 const Deposit: FunctionComponent<DepositProps> = ({ address }) => {
     // Global states
-    const { account, chain } = useWalletContext();
-    const metadata = Metadata[chain.id][address];
-    const provider = useProvider();
-    const [signerData] = useSigner();
+    const { account, chain, provider, signer } = useWalletContext();
+    const chainID = chain.unsupported ? DEFAULT_CHAIN.id : chain.chain.id;
+    const metadata = Metadata[chainID][address];
 
-    // Initialize contracts
-    const tokenContract = new ethers.Contract(metadata.debtAddress, erc20ABI, provider);
+    // Read onchain data
+    const allowanceResponse = useTokenAllowance({ account: account, token: metadata.debtAddress, spender: metadata.vaultAddress, provider: provider });
+    const balanceResponse = useTokenBalance({ account: account, token: metadata.debtAddress, provider: provider });
+
+    // Parse onchain data
+    const allowance = parseFloat(ethers.utils.formatUnits(allowanceResponse.data ? allowanceResponse.data : 0, metadata.debtDecimals));
+    const balance = parseFloat(ethers.utils.formatUnits(balanceResponse.data ? balanceResponse.data : 0, metadata.debtDecimals));
+
+    // Write operations
+    const [, approve] = useContractWrite(
+        {
+            addressOrName: metadata.debtAddress,
+            contractInterface: erc20ABI,
+            signerOrProvider: signer,
+        },
+        "approve",
+        {
+            args: [metadata.vaultAddress, ethers.constants.MaxUint256],
+        }
+    );
 
     // Local states
-    const [allowanceState, setAllowanceState] = useState<RequestState>({ loading: true });
-    const [balanceState, setBalanceState] = useState<RequestState>({ loading: true });
     const [approvalState, setApprovalState] = useState<ApprovalState>({});
 
-    // Load onchain data
-    const loadData = async () => {
-        if (allowanceState.loading) {
-            const values = await Promise.all([tokenContract.allowance(account, metadata.vaultAddress), tokenContract.balanceOf(account)]);
-            setAllowanceState({ response: values[0], loading: false });
-            setBalanceState({ response: values[1], loading: false });
-        }
-    };
-
-    // This will be executed when component is mounted or updated
-    useEffect(() => {
-        loadData();
-    });
-
     // UI States
-    const showLoading = allowanceState.loading || signerData.loading ? true : false;
-    const showError = !showLoading && allowanceState.error && signerData.error ? true : false;
-    const showApprovalOrDeposit = !showLoading && !showError && allowanceState.response ? true : false;
-    const showApproval = showApprovalOrDeposit && allowanceState.response && balanceState.response && signerData.data && !allowanceState.response.gt(balanceState.response) && !approvalState.approved ? true : false;
+    const showLoading = allowanceResponse.isLoading || balanceResponse.isLoading ? true : false;
+    const showError = !showLoading && allowanceResponse.error && allowanceResponse.error ? true : false;
+    const showApprovalOrDeposit = !showLoading && !showError && allowanceResponse.data && balanceResponse.data ? true : false;
+    const showApproval = showApprovalOrDeposit && !(allowance > balance) && !approvalState.approved ? true : false;
     const showDeposit = !showApproval || approvalState.approved ? true : false;
 
     return (
@@ -78,42 +81,47 @@ const Deposit: FunctionComponent<DepositProps> = ({ address }) => {
                                 <p className="text-center text-sm leading-6 text-gray-light-10 dark:text-gray-dark-10">Allow Risedle to use your {metadata.debtSymbol}</p>
                             </div>
                             <div className="pt-4">
-                                {!approvalState.approving && (
+                                {!approvalState.approving && !approvalState.confirming && (
                                     <ButtonPositive
                                         full
                                         onClick={async () => {
-                                            setApprovalState({ approving: true });
+                                            toast.remove(); // IMPORTANT this is used to prevent metamask popup stuck
+
                                             try {
-                                                if (!signerData.data) return setApprovalState({ approving: false });
-                                                const connectedContract = tokenContract.connect(signerData.data);
-                                                const result = await connectedContract.approve(metadata.vaultAddress, ethers.constants.MaxUint256);
-                                                setApprovalState({ approving: true, hash: result.hash });
-                                                toast.remove();
-                                                toast.custom((t) => <ToastInProgress>Approving {metadata.debtSymbol}</ToastInProgress>);
-                                                await result.wait();
+                                                setApprovalState({ confirming: true, approving: false });
+                                                const result = await approve();
+                                                setApprovalState({ confirming: false, approving: true });
+                                                if (result.error) {
+                                                    setApprovalState({ confirming: false, approving: false });
+                                                    toast.custom((t) => <ToastError>{result.error.message}</ToastError>);
+                                                    return;
+                                                }
+                                                setApprovalState({ confirming: false, approving: true, hash: result.data.hash });
+                                                toast.custom((t) => <ToastInProgress>Approving {metadata.debtSymbol}...</ToastInProgress>);
+                                                await result.data.wait();
                                                 toast.remove();
                                                 toast.custom((t) => <ToastSuccess>{metadata.debtSymbol} approved</ToastSuccess>);
-                                                setApprovalState({ approving: false, hash: result.hash, approved: true });
-                                                setAllowanceState({ loading: true }); // Reload component
+                                                setApprovalState({ approving: false, confirming: false, hash: result.data.hash, approved: true });
                                             } catch (e) {
+                                                // Wallet rejected etc
                                                 console.error(e);
                                                 const error = e as Error;
-                                                setApprovalState({ approving: false, error });
-                                                toast.remove();
-                                                toast.custom((t) => <ToastError>Approving failed</ToastError>);
+                                                setApprovalState({ confirming: false, approving: false, error });
+                                                toast.custom((t) => <ToastError>{error.message}</ToastError>);
                                             }
                                         }}
                                     >
                                         Approve
                                     </ButtonPositive>
                                 )}
+                                {approvalState.confirming && <ButtonLoading full>Waiting for confirmation...</ButtonLoading>}
                                 {approvalState.approving && <ButtonLoading full>Approving...</ButtonLoading>}
                             </div>
                             {approvalState.hash && (
                                 <div className="pt-4 text-center">
-                                    <Link href={getExplorerLink(chain, approvalState.hash)}>
+                                    <Link href={getExplorerLink(chain.chain, approvalState.hash)}>
                                         <a target="_blank" rel="noreferrer" className="text-gray-text-center py-4 text-sm text-sm leading-6 text-gray-light-10 dark:text-gray-dark-10">
-                                            <span className="hover:underline">Goto transaction</span> &#8599;
+                                            <span className="hover:underline">Transaction is submitted</span> &#8599;
                                         </a>
                                     </Link>
                                 </div>
