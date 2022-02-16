652eaedeb48f7cb9b3b14f1e47234172cf9f4ebb
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-02-16 13:18:13 +0700

    ✦ Delete unused file

--- components/v1/MarketDetailPage/Page.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from "react";
-
-export type PageProps = {};
-
-const Page: FunctionComponent<PageProps> = ({}) => {
-    return <div></div>;
-};
-
-export default Page;
