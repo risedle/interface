0387b5aff4472a8d7d38cc0431682cbf9f54aa9b
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-15 10:14:44 +0700

    Add Portofolio Page Route

---
+++ pages/portofolio/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+// Import components
+import PortofolioPage from "../../components/v1/PortofolioPage/PortofolioPage";
+
+const Home: NextPage = () => {
+    return <PortofolioPage />;
+};
+
+export default Home;
