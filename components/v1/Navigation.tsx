42aa64f29b828107e2e8661187fd5a853c91f310
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-16 22:23:10 +0700

    Create Navigation Component

---
+++ components/v1/Navigation.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import type { FunctionComponent } from "react";
+import ButtonConnectWalletDesktop from "./Buttons/ConnectWalletDesktop";
+import ButtonNetworkSwitcher from "./Buttons/NetworkSwitcher";
+import ButtonThemeSwitcher from "./Buttons/ThemeSwitcher";
+import Logo from "./Logo";
+
+/**
+ * NavigationProps is a React Component properties that passed to React Component Navigation
+ */
+type NavigationProps = {};
+
+/**
+ * Navigation is just yet another react component
+ *
+ * @link https://fettblog.eu/typescript-react/components/#functional-components
+ */
+const Navigation: FunctionComponent<NavigationProps> = ({}) => {
+    return (
+        <div className="container z-10 mx-auto max-w-full sm:z-20">
+            <div className="grid grid-cols-3 content-center place-items-center p-4">
+                <div className="justify-self-start">
+                    <Link href="/">
+                        <a className="flex items-center">
+                            <Logo />
+                            <span className="hidden sm:block traking-tight leading-0 self-center pl-2 font-inter text-base font-bold text-gray-light-12 dark:text-gray-light-1">Risedle</span>
+                        </a>
+                    </Link>
+                </div>
+                <div className="space-x-4 text-sm text-gray-light-12 dark:text-gray-dark-12">
+                    <Link href="/markets">
+                        <a>Markets</a>
+                    </Link>
+                    <Link href="/portofolio">
+                        <a>Portofolio</a>
+                    </Link>
+                </div>
+                <div className="justify-self-end inline-block flex flex-row space-x-2">
+                    <div className="hidden sm:inline-block">
+                        <ButtonNetworkSwitcher />
+                    </div>
+
+                    <div className="hidden sm:inline-block">
+                        <ButtonConnectWalletDesktop />
+                    </div>
+
+                    <div className="inline-block h-[40px]">
+                        <ButtonThemeSwitcher />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
