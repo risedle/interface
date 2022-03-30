dc3291efc98a14c4da406f103e2644d656dbbc6a
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-30 13:26:19 +0700

    Run Prettier Write

--- components/v1/PortofolioPage/NoPortofolioWarn.tsx
+++ components/v1/PortofolioPage/NoPortofolioWarn.tsx
@@ -7,29 +7,28 @@ type NoPorotoflioWarnProps = {
 const NoPorotoflioWarn = ({ type = "table" }: NoPorotoflioWarnProps) => {
     const { account } = useWalletContext();
 
-    if(type === "chart"){
-        return(
-            <div className="absolute inset-0 flex justify-center items-center z-10">
-                <div className="w-fit rounded-md px-4 py-2 bg-gray-light-3 dark:bg-gray-dark-3">
+    if (type === "chart") {
+        return (
+            <div className="absolute inset-0 z-10 flex items-center justify-center">
+                <div className="w-fit rounded-md bg-gray-light-3 px-4 py-2 dark:bg-gray-dark-3">
                     <h1 className="text-center text-xs text-gray-light-10">{account ? "You don't have any assets" : "Seems you're not connected to Risedle"}</h1>
                 </div>
             </div>
-        )
+        );
     } else {
         return (
             <div className="relative pt-[10px]">
-                <div className="space-y-[16px]" >
-                    <div className="bg-gray-light-3 dark:bg-gray-dark-3 rounded-full w-full h-[16px] opacity-40"></div>
-                    <div className="bg-gray-light-3 dark:bg-gray-dark-3 rounded-full w-full h-[16px] opacity-40"></div>
-                    <div className="bg-gray-light-3 dark:bg-gray-dark-3 rounded-full w-full h-[16px] opacity-40"></div>
+                <div className="space-y-[16px]">
+                    <div className="h-[16px] w-full rounded-full bg-gray-light-3 opacity-40 dark:bg-gray-dark-3"></div>
+                    <div className="h-[16px] w-full rounded-full bg-gray-light-3 opacity-40 dark:bg-gray-dark-3"></div>
+                    <div className="h-[16px] w-full rounded-full bg-gray-light-3 opacity-40 dark:bg-gray-dark-3"></div>
                 </div>
-                <div className="absolute inset-0 flex justify-center items-center z-10">
-                    <div className="w-fit rounded-md px-4 py-2 bg-gray-light-3 dark:bg-gray-dark-3">
+                <div className="absolute inset-0 z-10 flex items-center justify-center">
+                    <div className="w-fit rounded-md bg-gray-light-3 px-4 py-2 dark:bg-gray-dark-3">
                         <h1 className="text-center text-xs text-gray-light-10">{account ? "You don't have any assets" : "Seems you're not connected to Risedle"}</h1>
                     </div>
                 </div>
             </div>
-            
         );
     }
 };
