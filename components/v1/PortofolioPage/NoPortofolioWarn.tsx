d0c85793d6044ec0163e4c465badd4a93eb63fbd
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-30 13:04:54 +0700

    Refactor & Add Styling to NoPortofolioWarn

--- components/v1/PortofolioPage/NoPortofolioWarn.tsx
+++ components/v1/PortofolioPage/NoPortofolioWarn.tsx
@@ -7,13 +7,31 @@ type NoPorotoflioWarnProps = {
 const NoPorotoflioWarn = ({ type = "table" }: NoPorotoflioWarnProps) => {
     const { account } = useWalletContext();
 
-    return (
-        <div className={` ${type === "chart" && "absolute top-1/2 left-1/2  -translate-y-1/2 -translate-x-1/2  "} m-0 w-full text-center `}>
-            <div className="relative left-1/2 w-fit -translate-x-1/2 rounded-md px-4 py-2 dark:bg-gray-dark-3">
-                <h1 className="text-xs text-gray-light-10">{account ? "You don't have any assets" : "Seems you're not connected to Risedle"}</h1>
+    if(type === "chart"){
+        return(
+            <div className="absolute inset-0 flex justify-center items-center z-10">
+                <div className="w-fit rounded-md px-4 py-2 bg-gray-light-3 dark:bg-gray-dark-3">
+                    <h1 className="text-center text-xs text-gray-light-10">{account ? "You don't have any assets" : "Seems you're not connected to Risedle"}</h1>
+                </div>
             </div>
-        </div>
-    );
+        )
+    } else {
+        return (
+            <div className="relative pt-[10px]">
+                <div className="space-y-[16px]" >
+                    <div className="bg-gray-light-3 dark:bg-gray-dark-3 rounded-full w-full h-[16px] opacity-40"></div>
+                    <div className="bg-gray-light-3 dark:bg-gray-dark-3 rounded-full w-full h-[16px] opacity-40"></div>
+                    <div className="bg-gray-light-3 dark:bg-gray-dark-3 rounded-full w-full h-[16px] opacity-40"></div>
+                </div>
+                <div className="absolute inset-0 flex justify-center items-center z-10">
+                    <div className="w-fit rounded-md px-4 py-2 bg-gray-light-3 dark:bg-gray-dark-3">
+                        <h1 className="text-center text-xs text-gray-light-10">{account ? "You don't have any assets" : "Seems you're not connected to Risedle"}</h1>
+                    </div>
+                </div>
+            </div>
+            
+        );
+    }
 };
 
 export { NoPorotoflioWarn };
