fefff2b5e5d2e20e780f2671e5ddd35199a56891
Matthew Marcellino <81855912+math-marcellino@users.noreply.github.com>
2022-04-01 06:29:43 +0700

    ✦ Migrate Network Switcher to Radix UI (#160)
    Fix #156

--- components/v1/Buttons/NetworkSwitcher.tsx
+++ components/v1/Buttons/NetworkSwitcher.tsx
@@ -1,6 +1,6 @@
 import type { FunctionComponent } from "react";
 import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import { chain as Chains, Chain } from "wagmi";
 
@@ -26,8 +26,8 @@ const ButtonNetworkSwitcher: FunctionComponent<ButtonNetworkSwitcherProps> = ({}
     // Read global states
     const { account, chain, switchNetwork } = useWalletContext();
 
-    // Local states
-    let [isOpen, setIsOpen] = useState(false);
+    // Local state
+    const [isOpen, setIsOpen] = useState(false);
 
     const getChainIconPath = (c: Chain): string => {
         switch (c.id) {
@@ -54,70 +54,72 @@ const ButtonNetworkSwitcher: FunctionComponent<ButtonNetworkSwitcherProps> = ({}
             toast.custom((t) => <ToastError>Cannot switch network automatically in WalletConnect. Please change network directly from your wallet.</ToastError>);
             return;
         }
-
-        setIsOpen(false);
         return;
     };
 
     return (
         <>
-            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed inset-0 z-20 overflow-y-auto">
-                <div className="flex min-h-screen items-center justify-center">
-                    <Dialog.Overlay className="fixed inset-0 bg-gray-dark-1/60 backdrop-blur dark:bg-black/60" />
+            <Dialog.Root open={isOpen}>
+                <Dialog.Trigger>
+                    <button
+                        className="button basic p-0"
+                        onClick={() => {
+                            if (!account) {
+                                toast.remove();
+                                toast.custom((t) => <ToastError>Please connect your wallet first</ToastError>);
+                                return;
+                            }
+                            setIsOpen(true);
+                        }}
+                    >
+                        <img src={getChainIconPath(chain.chain)} alt={chain.chain.name} className="m-[11px] h-[16px] w-[16px]" />
+                    </button>
+                </Dialog.Trigger>
 
-                    <div className="relative mx-auto flex w-[342px] flex-col rounded-[24px] border border-gray-light-3 bg-gray-light-1 dark:border-gray-dark-3 dark:bg-gray-dark-1">
-                        <Dialog.Title className="m-0 flex flex-row items-center justify-between border-b border-dashed border-gray-light-3 px-4 py-4 dark:border-gray-dark-3">
-                            <span className="grow pl-[40px] text-center text-base font-bold leading-none text-gray-light-12 dark:text-gray-dark-12">Switch a Network</span>
-                            <ButtonClose
-                                onClick={() => {
-                                    setIsOpen(false);
-                                }}
-                            />
-                        </Dialog.Title>
+                <Dialog.Overlay className="fixed inset-0 bg-gray-dark-1/60 backdrop-blur dark:bg-black/60" />
 
-                        <div className="flex max-w-[342px] flex-col space-y-2 p-4">
-                            {supportedChains.map((c) => {
-                                return (
-                                    <button
-                                        className="m-0 flex w-full flex-row items-center space-x-4 rounded-[12px] border border-gray-light-5 bg-gray-light-2 py-[11px] px-[11px] text-left dark:border-gray-dark-5 dark:bg-gray-dark-2"
-                                        onClick={() => {
-                                            switchToNetwork(c);
-                                        }}
-                                        key={c.id}
-                                    >
-                                        <img src={getChainIconPath(c)} alt={c.name} className="h-[32px] w-[32px]" />
-                                        <span className="text-sm font-semibold leading-4 tracking-[-0.02em] text-gray-light-12 dark:text-gray-dark-12">{c.name}</span>
-                                    </button>
-                                );
-                            })}
-                        </div>
+                <Dialog.Content className="fixed inset-0 z-20 overflow-y-auto" asChild onPointerDownOutside={() => setIsOpen(false)} onInteractOutside={() => setIsOpen(false)}>
+                    <div className="flex min-h-screen items-center justify-center">
+                        <div className="relative mx-auto w-[342px] rounded-[24px] border border-gray-light-3 bg-gray-light-1 dark:border-gray-dark-3 dark:bg-gray-dark-1">
+                            <Dialog.Title className="m-0 flex flex-row items-center justify-between border-b border-dashed border-gray-light-3 px-4 py-4 dark:border-gray-dark-3">
+                                <span className="grow pl-[40px] text-center text-base font-bold leading-none text-gray-light-12 dark:text-gray-dark-12">Switch a Network</span>
+                                <Dialog.Close asChild>
+                                    <ButtonClose onClick={() => setIsOpen(false)} />
+                                </Dialog.Close>
+                            </Dialog.Title>
+                            <Dialog.Description>
+                                <div className="flex max-w-[342px] flex-col space-y-2 p-4">
+                                    {supportedChains.map((c) => {
+                                        return (
+                                            <button
+                                                className="m-0 flex w-full flex-row items-center space-x-4 rounded-[12px] border border-gray-light-5 bg-gray-light-2 py-[11px] px-[11px] text-left dark:border-gray-dark-5 dark:bg-gray-dark-2"
+                                                onClick={async () => {
+                                                    await switchToNetwork(c);
+                                                    setIsOpen(false);
+                                                }}
+                                                key={c.id}
+                                            >
+                                                <img src={getChainIconPath(c)} alt={c.name} className="h-[32px] w-[32px]" />
+                                                <span className="text-sm font-semibold leading-4 tracking-[-0.02em] text-gray-light-12 dark:text-gray-dark-12">{c.name}</span>
+                                            </button>
+                                        );
+                                    })}
+                                </div>
 
-                        <div className="m-0 border-t border-dashed border-gray-light-3 p-4 text-center dark:border-gray-dark-3">
-                            <p className="leading-1 text-xs text-gray-light-11 dark:text-gray-dark-11">
-                                Don&apos;t see you network? Chat us on{" "}
-                                <a className="text-gray-light-12 underline dark:text-gray-dark-12" target="_blank" rel="noreferrer" href={RisedleLinks.discord}>
-                                    discord
-                                </a>
-                                .
-                            </p>
+                                <div className="m-0 border-t border-dashed border-gray-light-3 p-4 text-center dark:border-gray-dark-3">
+                                    <p className="leading-1 text-xs text-gray-light-11 dark:text-gray-dark-11">
+                                        Don&apos;t see you network? Chat us on{" "}
+                                        <a className="text-gray-light-12 underline dark:text-gray-dark-12" target="_blank" rel="noreferrer" href={RisedleLinks.discord}>
+                                            discord
+                                        </a>
+                                        .
+                                    </p>
+                                </div>
+                            </Dialog.Description>
                         </div>
                     </div>
-                </div>
-            </Dialog>
-
-            <button
-                className="button basic p-0"
-                onClick={() => {
-                    if (!account) {
-                        toast.remove();
-                        toast.custom((t) => <ToastError>Please connect your wallet first</ToastError>);
-                        return;
-                    }
-                    setIsOpen(true);
-                }}
-            >
-                <img src={getChainIconPath(chain.chain)} alt={chain.chain.name} className="m-[11px] h-[16px] w-[16px]" />
-            </button>
+                </Dialog.Content>
+            </Dialog.Root>
         </>
     );
 };
