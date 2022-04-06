892edb5760ffaa484f06beee129d0002931e726f
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-04-06 22:04:25 +0700

    Fix OG Metadata Image Path

--- components/v1/PortfolioPage/PortfolioPageMeta.tsx
+++ components/v1/PortfolioPage/PortfolioPageMeta.tsx
@@ -1,35 +1,40 @@
 import type { FunctionComponent } from "react";
 
 /**
- * MarketsPageMetaProps is a React Component properties that passed to React Component MarketsPageMeta
+ * PortofolioPageMetaProps is a React Component properties that passed to React Component PortofolioPageMeta
  */
-type MarketsPageMetaProps = {};
+type PortofolioPageMetaProps = {};
 
 /**
- * MarketsPageMeta is just yet another react component
+ * PortofolioPageMeta is just yet another react component
  *
  * @link https://fettblog.eu/typescript-react/components/#functional-components
  */
 
-const MarketsPageMeta: FunctionComponent<MarketsPageMetaProps> = ({}) => {
+const PortofolioPageMeta: FunctionComponent<PortofolioPageMetaProps> = ({}) => {
     return (
         <>
+            {/* 
+                This component is reusable to generate og metadata for each ETF asset available.
+                Note: The description is currently hard coded and will be changed when a new ETF available
+             */}
+
             {/* <!-- Facebook Meta Tags --> */}
-            <meta property="og:url" content="https://risedle.com/" />
+            <meta property="og:url" content="https://risedle.com/portfolio" />
             <meta property="og:type" content="website" />
             <meta property="og:title" content="Risedle Protocol" />
             <meta property="og:description" content="Invest, earn and build on the decentralized crypto leveraged ETFs market protocol" />
-            <meta property="og:image" content="/assets/images/og/Markets.png" />
+            <meta property="og:image" content="/assets/images/og/Portfolio.png" />
 
             {/* <!-- Twitter Meta Tags --> */}
             <meta name="twitter:card" content="summary_large_image" />
             <meta property="twitter:domain" content="risedle.com" />
-            <meta property="twitter:url" content="https://risedle.com/" />
+            <meta property="twitter:url" content="https://risedle.com/portofolio" />
             <meta name="twitter:title" content="Risedle Protocol" />
             <meta name="twitter:description" content="Invest, earn and build on the decentralized crypto leveraged ETFs market protocol" />
-            <meta name="twitter:image" content="/assets/images/og/Markets.png" />
+            <meta name="twitter:image" content="/assets/images/og/Portfolio.png" />
         </>
     );
 };
 
-export default MarketsPageMeta;
+export default PortofolioPageMeta;
--- components/v1/PortfolioPage/PortfolioPageV2.tsx
+++ components/v1/PortfolioPage/PortfolioPageV2.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import Head from "next/head";
-import MarketsPageMeta from "../MarketsPage/MarketsPageMeta";
+import PortofolioPageMeta from "./PortfolioPageMeta";
 import Favicon from "../Favicon";
 import ButtonConnectWalletMobile from "../Buttons/ConnectWalletMobile";
 import BackgroundGradient from "./BackgroundGradient";
@@ -81,7 +81,7 @@ const PortfolioPageV2: FunctionComponent<PortfolioPageV2Props> = ({}) => {
                 {/* <!-- HTML Meta Tags --> */}
                 <title>Risedle Protocol</title>
                 <meta name="description" content="Invest, earn and build on the decentralized crypto leveraged ETFs market protocol" />
-                <MarketsPageMeta />
+                <PortofolioPageMeta />
             </Head>
             <Favicon />
 
