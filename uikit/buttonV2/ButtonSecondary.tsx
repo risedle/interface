2ac878730bc6ef3d671e7ad06c55e5b9ab4c664c
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-05-20 15:37:20 +0700

    style: fix tailwind values

--- uikit/buttonV2/ButtonSecondary.tsx
+++ uikit/buttonV2/ButtonSecondary.tsx
@@ -20,17 +20,17 @@ const ButtonSecondary: FunctionComponent<ButtonSecondaryProps> = ({ onClick, siz
     let extraClass = "";
     switch (size) {
         case "md":
-            extraClass = "px-[16px] heading-h7";
+            extraClass = "px-4 heading-h7";
             break;
         case "lg":
-            extraClass = "px-[20px] heading-h6";
+            extraClass = "px-5 heading-h6";
             break;
         case "xl":
-            extraClass = "px-[24px] heading-h6";
+            extraClass = "px-6 heading-h6";
             break;
     }
     return (
-        <button className={`flex flex-row items-center justify-center gap-[8px] ${extraClass} bg-light-neutral-subtle/10 py-[12px] text-dark-neutral-primary ${type == "square" ? "rounded-[8px]" : "rounded-full"}`} onClick={onClick}>
+        <button className={`flex flex-row items-center justify-center gap-2 ${extraClass} bg-light-neutral-subtle/10 py-3 text-dark-neutral-primary ${type == "square" ? "rounded-lg" : "rounded-full"}`} onClick={onClick}>
             {children}
         </button>
     );
