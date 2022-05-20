925a9f3c27a203c61adeebcd83e856a1c63c2184
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-05-20 14:13:43 +0700

    style: setup tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,33 @@ module.exports = {
                 ibm: ["'IBM Plex Mono'", "monospace"],
             },
             colors: {
+                dark: {
+                    neutral: "#FCFDFF",
+                    primary: "#EC38BC",
+                    secondary: "#7303C0",
+                    background: {
+                        default: "#03050D",
+                        elevated: "#13151F",
+                    },
+                    neutral: {
+                        primary: "#FCFDFF",
+                        medium: "#ECEDF2",
+                        soft: "#8E93AF",
+                        subtle: "#2B2E3F",
+                    },
+                },
+                light: {
+                    background: "#FCFDFF",
+                    neutral: "#070D2D",
+                    primary: "#7303C0",
+                    secondary: "#EC38BC",
+                    neutral: {
+                        primary: "#070D2D",
+                        medium: "#535978",
+                        soft: "#9198BD",
+                        subtle: "#EEF0FC",
+                    },
+                },
                 blue: {
                     DEFAULT: "#2D81FF",
                 },
