e4698643e0e37c036ebfc5dd543a928162bcb919
Adam Akbar <adam.akbar@ruangguru.com>
2022-06-12 13:48:20 +0700

    feature jumbotron4

--- modules/homePage/component/v2/FourthJumbotron/FourthJumbotron.tsx
+++ modules/homePage/component/v2/FourthJumbotron/FourthJumbotron.tsx
@@ -3,29 +3,29 @@ import { ForthJumobtronBox, ForthJumobtronTitle, Image, Button } from "./FourthJ
 
 export default function FourthJumbotron() {
     return (
-        <div className="mx-auto max-w-7xl pt-28">
-            <h1 className="text-[148px] font-extrabold tracking-tight text-dark-neutral-primary	">
+        <div className="mx-auto max-w-[450px] px-4 text-center sm:max-w-[552px] sm:text-left md:max-w-[700px] lg:max-w-[936px] xl:max-w-[1128px]">
+            <h1 className="mb-6 text-center text-[64px] font-extrabold leading-[72px] tracking-[-0.02em] text-dark-neutral-primary md:text-left lg:text-[148px]	lg:leading-[136px]">
                 Dive Deeper <br /> Into Risedle
             </h1>
-            <h3 className="paragraph-xl text-dark-neutral-soft">
+            <h3 className="paragraph-xl mb-8 text-dark-neutral-soft">
                 Read our docs, join our Discord, or jump into our <br />
                 Twitter account to get more learning resources!
             </h3>
             <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
                 <ForthJumobtronBox className="col-span-1">
                     <ForthJumobtronTitle>Documentation</ForthJumobtronTitle>
-                    <Image src="/assets/images/homepage/section-4-1.svg" />
-                    <Button>Read Docs</Button>
+                    <Image src="/assets/images/homepage/doc.png" alt="docs" />
+                    <Button className="bg-dark-primary ">Read Docs</Button>
                 </ForthJumobtronBox>
                 <ForthJumobtronBox className="col-span-1 ">
-                    <ForthJumobtronTitle>Documentation</ForthJumobtronTitle>
-                    <Image src="/assets/images/homepage/section-4-3.svg" />
-                    <Button>Join Discord</Button>
+                    <ForthJumobtronTitle>Discord Server</ForthJumobtronTitle>
+                    <Image src="/assets/images/homepage/discord.png" alt="discord" />
+                    <Button className="bg-[#5B64EA]">Join Discord</Button>
                 </ForthJumobtronBox>
-                <ForthJumobtronBox className="md:col-span-2 lg:col-span-1 	">
-                    <ForthJumobtronTitle>Documentation</ForthJumobtronTitle>
-                    <Image src="/assets/images/homepage/section-4-2.svg" />
-                    <Button>Follow @risedle</Button>
+                <ForthJumobtronBox className="md:col-span-2 lg:col-span-1">
+                    <ForthJumobtronTitle>Twitter</ForthJumobtronTitle>
+                    <Image src="/assets/images/homepage/Twitter.png" alt="twitter" />
+                    <Button className="bg-[#3292D8]">Follow @risedle</Button>
                 </ForthJumobtronBox>
             </div>
         </div>
