51a0cd1c83b1f9aabcd1f155463c302c6d66fc4a
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-12 18:09:20 +0700

    feat: implement gradient (temporary)

--- modules/homePage/component/v2/ThridJumbotron/Gradients.tsx
+++ modules/homePage/component/v2/ThridJumbotron/Gradients.tsx
@@ -1,23 +1,23 @@
 import type { FunctionComponent } from "react";
 
-/**
- * GradientsProps is a React Component properties that passed to React Component Gradients
- */
-type GradientsProps = {};
-
-/**
- * Gradients is just yet another react component
- *
- * @link https://fettblog.eu/typescript-react/components/#functional-components
- */
-const Gradients: FunctionComponent<GradientsProps> = ({}) => {
-    return (
-        <div className="flex flex-col items-center justify-center gap-[800px] bg-dark-background-default pt-32">
-            <div className="color-product-ethrise-gradient h-[523px] w-[339px] rotate-[-52.5deg] blur-[269px]"></div>
-            <div className="color-product-avaxrise-gradient h-[404px] w-[321px] rotate-[-79deg] rounded-[176px] opacity-50 blur-[100px]"></div>
-            <div className="color-product-gohmrise-gradient h-[906px] w-[447px] rotate-[-77.85deg] rounded-[216px] opacity-[0.4] blur-[268px]"></div>
-        </div>
-    );
+type EthriseGradientsProps = {
+    className?: string;
+};
+type AvaxriseGradientsProps = {
+    className?: string;
+};
+type GohmriseGradientsProps = {
+    className?: string;
 };
 
-export default Gradients;
+export const EthriseGradients: FunctionComponent<EthriseGradientsProps> = ({ className }) => {
+    return <div className={`color-product-ethrise-gradient h-[523.05px] w-[339.24px] rotate-[-52.5deg] blur-[269px] ${className || ""}`}></div>;
+};
+
+export const AvaxriseGradients: FunctionComponent<AvaxriseGradientsProps> = ({ className }) => {
+    return <div className={`color-product-avaxrise-gradient h-[404px] w-[321.32px] rotate-[-79.02deg] rounded-[176px] opacity-50 blur-[100px] ${className || ""}`}></div>;
+};
+
+export const GohmriseGradients: FunctionComponent<GohmriseGradientsProps> = ({ className }) => {
+    return <div className={`color-product-gohmrise-gradient h-[906px] w-[447px] rotate-[-77.85deg] rounded-[216px] opacity-[0.4] blur-[268px] ${className || ""}`}></div>;
+};
--- modules/homePage/component/v2/ThridJumbotron/ThridJumbotron.tsx
+++ modules/homePage/component/v2/ThridJumbotron/ThridJumbotron.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from "react";
+import { EthriseGradients, AvaxriseGradients, GohmriseGradients } from "./Gradients";
 import ButtonPrimary from "../../../../../uikit/buttonV2/ButtonPrimary";
 
 /**
@@ -13,33 +14,38 @@ type ThridJumbotronProps = {};
  */
 const ThridJumbotron: FunctionComponent<ThridJumbotronProps> = ({}) => {
     return (
-        <div className="mx-auto flex max-w-[343px] flex-col items-center justify-between gap-[72px] py-32 sm:max-w-[552px] lg:max-w-[936px] lg:flex-row lg:items-start xl:max-w-[1128px]">
-            <div className="flex flex-col items-center gap-16 lg:items-start">
-                <div className="flex flex-col items-center gap-6 lg:items-start">
-                    <p className="xl:heading-h0 heading-h1 text-center text-dark-neutral-primary drop-shadow-sm sm:whitespace-nowrap lg:whitespace-normal lg:text-left">Reliable & Trusted</p>
-                    <p className="xl:paragraph-xl lg:paragraph-l paragraph-m text-center text-dark-neutral-soft lg:text-left">Risedle protocol working smooth as a butter in the markets. Running like a porsche.</p>
+        <div className="relative">
+            <div className="mx-auto flex max-w-[343px] flex-col items-center justify-between gap-[72px] py-32 sm:max-w-[552px] lg:max-w-[936px] lg:flex-row lg:items-start xl:max-w-[1128px]">
+                <div className="z-20 flex flex-col items-center gap-16 lg:items-start">
+                    <div className="flex flex-col items-center gap-6 lg:items-start">
+                        <p className="xl:heading-h0 heading-h1 text-center text-dark-neutral-primary drop-shadow-sm sm:whitespace-nowrap lg:whitespace-normal lg:text-left">Reliable & Trusted</p>
+                        <p className="xl:paragraph-xl lg:paragraph-l paragraph-m text-center text-dark-neutral-soft lg:text-left">Risedle protocol working smooth as a butter in the markets. Running like a porsche.</p>
+                    </div>
+                    <ButtonPrimary size="xl" type="default" className="hidden w-fit lg:flex">
+                        Open Position
+                    </ButtonPrimary>
                 </div>
-                <ButtonPrimary size="xl" type="default" className="hidden w-fit lg:flex">
+                <div className="z-20 flex flex-row flex-wrap justify-center gap-[72px] lg:flex-col">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <p className="text-xs uppercase tracking-widest text-dark-neutral-soft">Trading Volume 24H</p>
+                        <p className="lg:display-d2 display-d3 text-dark-neutral-primary">$2,234,234</p>
+                    </div>
+                    <div className="flex flex-col items-center lg:items-start">
+                        <p className="text-xs uppercase tracking-widest text-dark-neutral-soft">Transaction 24H</p>
+                        <p className="lg:display-d2 display-d3 text-dark-neutral-primary">$2,234,234</p>
+                    </div>
+                    <div className="flex flex-col items-center lg:items-start">
+                        <p className="text-xs uppercase tracking-widest text-dark-neutral-soft">Total AUM 24H</p>
+                        <p className="lg:display-d2 display-d3 text-dark-neutral-primary">$22,234,234</p>
+                    </div>
+                </div>
+                <ButtonPrimary size="lg" type="default" className="z-20 lg:hidden">
                     Open Position
                 </ButtonPrimary>
             </div>
-            <div className="flex flex-row flex-wrap justify-center gap-[72px] lg:flex-col">
-                <div className="flex flex-col items-center lg:items-start">
-                    <p className="text-xs uppercase tracking-widest text-dark-neutral-soft">Trading Volume 24H</p>
-                    <p className="lg:display-d2 display-d3 text-dark-neutral-primary">$2,234,234</p>
-                </div>
-                <div className="flex flex-col items-center lg:items-start">
-                    <p className="text-xs uppercase tracking-widest text-dark-neutral-soft">Transaction 24H</p>
-                    <p className="lg:display-d2 display-d3 text-dark-neutral-primary">$2,234,234</p>
-                </div>
-                <div className="flex flex-col items-center lg:items-start">
-                    <p className="text-xs uppercase tracking-widest text-dark-neutral-soft">Total AUM 24H</p>
-                    <p className="lg:display-d2 display-d3 text-dark-neutral-primary">$22,234,234</p>
-                </div>
-            </div>
-            <ButtonPrimary size="lg" type="default" className="lg:hidden">
-                Open Position
-            </ButtonPrimary>
+            <EthriseGradients className="absolute -right-[500px] -top-4 xl:-right-96 xl:-top-16 2xl:-right-44 2xl:-top-28" />
+            <GohmriseGradients className="absolute -bottom-24 -left-[500px] lg:-left-36 lg:-bottom-44" />
+            <AvaxriseGradients className="absolute hidden lg:-left-36 lg:-bottom-20 lg:block" />
         </div>
     );
 };
