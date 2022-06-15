c10f83ea05facb9b888cda22795ed9dc43df98bd
Adam Akbar <adam.akbar@ruangguru.com>
2022-06-15 23:24:58 +0700

    fix border radius

--- modules/homePage/component/v2/FourthJumbotron/FourthJumbotronComponent.tsx
+++ modules/homePage/component/v2/FourthJumbotron/FourthJumbotronComponent.tsx
@@ -1,7 +1,7 @@
 import ButtonPrimary from "../../../../../uikit/buttonV2/ButtonPrimary";
 
 function ForthJumobtronBox({ children, className }: { className?: string; children?: React.ReactNode }) {
-    return <div className={`relative mt-4 h-[236px] max-w-full overflow-hidden rounded-[36px] bg-dark-background-elevated pt-8 pl-8 text-left sm:h-[280px] xl:h-[300px]     ${className || ""}`}>{children}</div>;
+    return <div className={`relative mt-4 h-[236px] max-w-full overflow-hidden rounded-3xl bg-dark-background-elevated pt-8 pl-8 text-left sm:h-[280px] xl:h-[300px]     ${className || ""}`}>{children}</div>;
 }
 
 function ForthJumobtronTitle({ children }: { children?: React.ReactNode }) {
@@ -11,8 +11,8 @@ function ForthJumobtronTitle({ children }: { children?: React.ReactNode }) {
 function Image({ src, alt }: { src: string; alt: string }) {
     return (
         <>
-            <img src={src} alt={alt} className="absolute left-[32px] bottom-[-80px] h-full w-full  rounded-lg rounded-br-[36px] sm:bottom-0 sm:h-[210px]  " />
-            <div style={{ background: "linear-gradient(0deg, #0E1018 0%, rgba(14, 16, 24, 0) 100%)" }} className="absolute bottom-0 left-0 h-full w-full rounded-[36px]"></div>
+            <img src={src} alt={alt} className="absolute left-[32px] bottom-[-80px] h-full w-full  rounded-lg sm:bottom-0 sm:h-[210px]  " />
+            <div style={{ background: "linear-gradient(0deg, #0E1018 0%, rgba(14, 16, 24, 0) 100%)" }} className="absolute bottom-0 left-0 h-full w-full rounded-3xl"></div>
         </>
     );
 }
