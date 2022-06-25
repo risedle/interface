5b9b487dc08c9cafbe70c33148ebe44fa60d1b6b
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-25 19:33:39 +0700

    feat: add bsc warning modal

--- uikit/button/ButtonPrimary.tsx
+++ uikit/button/ButtonPrimary.tsx
@@ -18,7 +18,7 @@ type ButtonPrimaryProps = {
 const ButtonPrimary: FunctionComponent<ButtonPrimaryProps> = ({ onClick, full, children }) => {
     return (
         <button
-            className={`button rounded-full border border-blue-light-11 bg-blue-light-10 py-[12px] px-[24px] text-sm font-semibold leading-4 tracking-[-0.02em] text-gray-light-1 transition duration-300 ease-out hover:border-blue-light-10 hover:bg-blue-light-9 active:border-blue-light-9 active:bg-blue-light-8 dark:border-blue-dark-11 dark:bg-blue-dark-10 dark:text-blue-light-1 hover:dark:border-blue-dark-10 hover:dark:bg-blue-dark-9 active:dark:border-blue-dark-9 active:dark:bg-blue-dark-8 ${
+            className={`button h-10 rounded-full border border-blue-light-11 bg-blue-light-10 px-6 text-sm font-semibold leading-4 tracking-[-0.02em] text-gray-light-1 transition duration-300 ease-out hover:border-blue-light-10 hover:bg-blue-light-9 active:border-blue-light-9 active:bg-blue-light-8 dark:border-blue-dark-11 dark:bg-blue-dark-10 dark:text-blue-light-1 hover:dark:border-blue-dark-10 hover:dark:bg-blue-dark-9 active:dark:border-blue-dark-9 active:dark:bg-blue-dark-8 ${
                 full ? "w-full" : ""
             }`}
             onClick={onClick}
