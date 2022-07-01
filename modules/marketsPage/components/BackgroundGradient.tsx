66f8d4022648f75f4d93caa533b79b68b89d385a
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-07-01 16:37:49 +0700

    style: fix bg gradient positioning (markets)

--- modules/marketsPage/components/BackgroundGradient.tsx
+++ modules/marketsPage/components/BackgroundGradient.tsx
@@ -19,7 +19,7 @@ const BackgroundGradient: FunctionComponent<BackgroundGradientProps> = ({ chainI
     const { chain } = useWalletContext();
     return (
         <>
-            <div className="absolute -top-[56%] left-1/2 -translate-x-1/2 sm:-top-1/2">
+            <div className="absolute top-0 left-1/2 -translate-y-2/3 -translate-x-1/2">
                 <svg className="stroke-black dark:stroke-white" width="679" height="679" viewBox="0 0 679 679" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g opacity="0.5">
                         <circle opacity="0.1" cx="339.5" cy="339.5" r="130.173" />
@@ -30,13 +30,13 @@ const BackgroundGradient: FunctionComponent<BackgroundGradientProps> = ({ chainI
                 </svg>
             </div>
             {chainID === customChains.bsc.id && (
-                <div className="absolute -top-3/4 left-1/2 h-max w-max -translate-x-1/2 sm:-top-2/3">
+                <div className="absolute top-0 left-1/2 h-max w-max -translate-x-1/2 -translate-y-[60%]">
                     <img src="/assets/images/marketspage/gradient-bsc-dark.svg" className="hidden dark:block" />
                     <img src="/assets/images/marketspage/gradient-bsc-light.svg" className="dark:hidden" />
                 </div>
             )}
             {chainID === Chains.arbitrumOne.id && (
-                <div className="absolute -top-3/4 left-1/2 h-max w-max -translate-x-1/2 sm:-top-2/3">
+                <div className="absolute top-0 left-1/2 h-max w-max -translate-x-1/2 -translate-y-[60%]">
                     <img src="/assets/images/marketspage/gradient-arb-dark.svg" className="hidden dark:block" />
                     <img src="/assets/images/marketspage/gradient-arb-light.svg" className="dark:hidden" />
                 </div>
